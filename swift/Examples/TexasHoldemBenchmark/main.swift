2e7d127379db71694762dee659ac1b95511beeca
OpenSpiel Development Team <open_spiel@google.com>
2019-08-29 07:07:36 -0600

    Update the Texas Hold'em benchmark.
    Based on profiling, we can play a full round of Texas Hold'em in
    approximately 8.4 micro-seconds (with the vast majority of the time spent in
    computing the winning hand, indicating a potential direction for future
    optimizations).
    
    Note: the previous implementation of the benchmark was dominated by
    overheads in the random play. This shows that the implementation of the game
    engine itself is extremely efficient, and it's up to users to choose which
    level of abstraction they would like to program against.
    PiperOrigin-RevId: 266123645

--- swift/Examples/TexasHoldemBenchmark/main.swift
+++ swift/Examples/TexasHoldemBenchmark/main.swift
@@ -15,6 +15,11 @@
 /// This file executes repeated random play over the Texas Hold'em game.
 ///
 /// It is used for profiling and benchmarking purposes.
+///
+/// On saeta@'s desktop machine, performance numbers are as follows (be sure to
+/// build with optimizations on!):
+///   - Random games: 115 +/- 51 usec per game
+///   - Fixed games:  8.4 +/- 0.5 usec per game
 
 import Dispatch
 import Foundation
@@ -28,8 +33,8 @@ func time(_ benchmarkName: String, _ f: () -> ()) {
 
   var times = [Double]()
 
-  // Play 100 games.
-  for _ in 1...1000 {
+  // Play 10000 games.
+  for _ in 1...10000 {
     let start = DispatchTime.now()
     f()
     let end = DispatchTime.now()
@@ -46,6 +51,12 @@ func time(_ benchmarkName: String, _ f: () -> ()) {
 }
 
 
+/// Play games at random to see
+///
+/// Note: this benchmark is dominated by the overheads of random number generation
+/// and constructing collection manipulations (specifically around chance nodes, as
+/// there are a lot more cards than player actions, so these tend to dominate the
+/// runtime).
 func playRandomGame() {
   let game = TexasHoldem(playerCount: 2)
   var state = game.initialState
@@ -54,6 +65,47 @@ func playRandomGame() {
   }
 }
 
-print("About to start playing random games...")
+/// Play a fixed game to benchmark the overheads of the state mechanisms itself.
+///
+/// Note: based on profiling, the vast majority of the time is actually spent in computeBestHand
+/// with the rest of the time spent in either array reallocation or swift_beginAccess (these
+/// dynamic safety checks can be disabled with additional optimization flags).
+func playFixed2PlayerGame() {
+  let game = TexasHoldem(playerCount: 2)
+  var state = game.initialState
+  // Deal 4 cards (this deck happens to be relatively poorly shuffled...)
+  state.apply(.card(Card(suit: .clubs, rank: .two)))
+  state.apply(.card(Card(suit: .clubs, rank: .three)))
+  state.apply(.card(Card(suit: .clubs, rank: .four)))
+  state.apply(.card(Card(suit: .clubs, rank: .five)))
+
+  // Pre-flop
+  state.apply(.call)
+  state.apply(.call)
+
+  // Deal flop
+  state.apply(.card(Card(suit: .clubs, rank: .six)))
+  state.apply(.card(Card(suit: .clubs, rank: .seven)))
+  state.apply(.card(Card(suit: .clubs, rank: .eight)))
+
+  // Bid
+  state.apply(.raise(1))
+  state.apply(.raise(1))
+  state.apply(.call)
+
+  // Turn & bid
+  state.apply(.card(Card(suit: .clubs, rank: .nine)))
+  state.apply(.call)
+  state.apply(.raise(2))
+  state.apply(.call)
+
+  // River & bid
+  state.apply(.card(Card(suit: .clubs, rank: .ten)))
+  state.apply(.allIn)
+  state.apply(.call)
+}
+
+print("About to start benchmarks...")
 time("Random 2-player Texas Hold'em games", playRandomGame)
+time("Fixed 2-player Texas Hold'em games", playFixed2PlayerGame)
 print("Done!")
