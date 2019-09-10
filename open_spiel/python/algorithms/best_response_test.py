57008f9e2a78a7730d783bfcc15b1fb9485e33bf
DeepMind Technologies Ltd <open_spiel@google.com>
2019-09-10 10:10:23 -0600

    Move the BestResponse out of exploitability, to be a first class citizen Policy.
    We often use Best responses but we do not have any clean way of getting a `Policy` object being a BR. We often have to combine `PolicyFromCallable` with the available ways to compute a BR.
    
    Also, the BR is computed in several places (e.g. in exploitability.py, and also in action_value_vs_best_response.py, where it uses the C++ implementation.
    
    Having a single entry-point would make it simpler to call the C++ version whenever we discover we can, e.g. because the provided policy is a TabularPolicy.
    
    PiperOrigin-RevId: 268238420
    Change-Id: I0c11d480661263e0e9eb2a1217203d1bdc1e5f9c

---
+++ open_spiel/python/algorithms/best_response_test.py
@@ -0,0 +1,53 @@
+# Copyright 2019 DeepMind Technologies Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""Tests for google3.third_party.open_spiel.python.algorithms.best_response."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from google3.testing.pybase import googletest
+from open_spiel.python import policy
+from open_spiel.python.algorithms import best_response as pyspiel_best_response
+import pyspiel
+
+
+class BestResponseTest(googletest.TestCase):
+
+  def test_best_response_is_a_policy(self):
+    game = pyspiel.load_game("kuhn_poker")
+    test_policy = policy.UniformRandomPolicy(game)
+    best_response = pyspiel_best_response.BestResponsePolicy(
+        game, policy=test_policy, player_id=0)
+    expected_policy = {
+        "0": 1,  # Bet in case opponent folds when winning
+        "1": 1,  # Bet in case opponent folds when winning
+        "2": 0,  # Both equally good (we return the lowest action)
+        # Some of these will never happen under the best-response policy,
+        # but we have computed best-response actions anyway.
+        "0pb": 0,  # Fold - we're losing
+        "1pb": 1,  # Call - we're 50-50
+        "2pb": 1,  # Call - we've won
+    }
+    self.assertEqual(
+        expected_policy, {
+            key: best_response.best_response_action(key)
+            for key in expected_policy.keys()
+        })
+
+
+if __name__ == "__main__":
+  googletest.main()
