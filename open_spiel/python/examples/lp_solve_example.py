89b65ac16ff906326d426c7e894130a433e6ad5a
DeepMind Technologies Ltd <open_spiel@google.com>
2019-09-23 11:32:23 -0600

    General-sum Nash solver example, using iterated dominance and multiple solution methods.
    Methods:
    * checking for pure equilibria
    * LP for constant-sum games
    * nashpy implementations of Lemke-Howson and vertex enumeration
    * lrslib implementation of reverse search vertex enumeration (using an external binary)
    PiperOrigin-RevId: 270707582
    Change-Id: I860887ce60c0a6a90beffa6e3f0f1cca58da492b

--- open_spiel/python/examples/lp_solve_example.py
+++ open_spiel/python/examples/lp_solve_example.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Python spiel example."""
+"""Solving matrix games with LP solver."""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -32,14 +32,12 @@ def main(_):
           [[0.0, 0.25, -0.5], [-0.25, 0.0, 0.05], [0.5, -0.05, 0.0]]))
   print("p0 val = {}, policy = {}".format(p0_sol_val, p0_sol))
   print("p1 val = {}, policy = {}".format(p1_sol_val, p1_sol))
-  print(p0_sol[1])
 
+  payoff_matrix = [[1., 1., 1.], [2., 0., 1.], [0., 2., 2.]]
   mixture = lp_solver.is_dominated(
-      0, [[1., 1., 1.], [2., 0., 1.], [0., 2., 2.]],
-      0,
-      lp_solver.DOMINANCE_WEAK,
-      return_mixture=True)
-  print(mixture)
+      0, payoff_matrix, 0, lp_solver.DOMINANCE_WEAK, return_mixture=True)
+  print("mixture strategy : {}".format(mixture))
+  print("payoff vector    : {}".format(mixture.dot(payoff_matrix)))
 
 
 if __name__ == "__main__":
