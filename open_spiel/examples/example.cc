d869ccf8c41a85de4b8b63ed8c8ef11966d1b786
DeepMind Technologies Ltd <open_spiel@google.com>
2019-10-07 12:13:39 -0600

    Switch to ABSL flags.
    The existing flag library only supports `--flag=value`, not `--flag value`, which is mighty confusing. Also, it doesn't have great support for types.
    
    PiperOrigin-RevId: 273333938
    Change-Id: Ia31bf98287d0608d39f1d26e623843aae9f31ea8

--- open_spiel/examples/example.cc
+++ open_spiel/examples/example.cc
@@ -17,13 +17,17 @@
 #include <memory>
 #include <random>
 
+#include "open_spiel/abseil-cpp/absl/flags/flag.h"
+#include "open_spiel/abseil-cpp/absl/flags/parse.h"
 #include "open_spiel/abseil-cpp/absl/random/uniform_int_distribution.h"
 #include "open_spiel/spiel.h"
 #include "open_spiel/spiel_utils.h"
 
-const char* kUsageStr =
-    "example --game=<shortname> [--players=<num>] "
-    "[--show_infostate] [--seed=<num>] [--show_legals=<true/false>]";
+ABSL_FLAG(std::string, game, "tic_tac_toe", "The name of the game to play.");
+ABSL_FLAG(int, players, 0, "How many players in this game, 0 for default.");
+ABSL_FLAG(bool, show_infostate, false, "Show the information state.");
+ABSL_FLAG(int, seed, 0, "Seed for the random number generator. 0 for auto.");
+ABSL_FLAG(bool, show_legals, false, "Show the legal moves.");
 
 void PrintLegalActions(const open_spiel::State& state,
                        open_spiel::Player player,
@@ -35,16 +39,13 @@ void PrintLegalActions(const open_spiel::State& state,
 }
 
 int main(int argc, char** argv) {
-  std::string game_name =
-      open_spiel::ParseCmdLineArgDefault(argc, argv, "game", "");
-  auto players = open_spiel::Player{std::stoi(
-      open_spiel::ParseCmdLineArgDefault(argc, argv, "players", "0"))};
-  bool show_infostate = open_spiel::ParseCmdLineArgDefault(
-                            argc, argv, "show_infostate", "false") == "true";
-  std::pair<bool, std::string> seed =
-      open_spiel::ParseCmdLineArg(argc, argv, "seed");
-  bool show_legals = open_spiel::ParseCmdLineArgDefault(
-                         argc, argv, "show_legals", "false") == "true";
+  absl::ParseCommandLine(argc, argv);
+
+  std::string game_name = absl::GetFlag(FLAGS_game);
+  auto players = absl::GetFlag(FLAGS_players);
+  bool show_infostate = absl::GetFlag(FLAGS_show_infostate);
+  int seed = absl::GetFlag(FLAGS_seed);
+  bool show_legals = absl::GetFlag(FLAGS_show_legals);
 
   // Print out registered games.
   std::cerr << "Registered games:" << std::endl;
@@ -53,13 +54,8 @@ int main(int argc, char** argv) {
     std::cerr << name << std::endl;
   }
 
-  if (game_name.empty()) {
-    std::cerr << kUsageStr << std::endl;
-    return -1;
-  }
-
   // Random number generator.
-  std::mt19937 rng(seed.first ? std::stol(seed.second) : time(0));
+  std::mt19937 rng(seed ? seed : time(0));
 
   // Create the game.
   std::cerr << "Creating game..\n" << std::endl;
