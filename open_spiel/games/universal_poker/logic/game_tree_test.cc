77a940f9bf4f1ac4be9eb079cbdd9243f176f458
Dennis Jöst <djprivat@gmail.com>
2019-10-20 18:16:49 +0200

    Another test

--- open_spiel/games/universal_poker/logic/game_tree_test.cc
+++ open_spiel/games/universal_poker/logic/game_tree_test.cc
@@ -7,14 +7,15 @@
 namespace open_spiel::universal_poker::logic {
 
 void BasicGameTreeTests() {
-    const std::string gameDesc("GAMEDEF\nnolimit\nnumPlayers = 2\nnumRounds = 2\nstack = 1200 1200\nblind = 100 100\nfirstPlayer = 1 1\nnumSuits = 2\nnumRanks = 3\nnumHoleCards = 1\nnumBoardCards = 0 1\nEND GAMEDEF");
+    const std::string gameDesc(
+            "GAMEDEF\nnolimit\nnumPlayers = 2\nnumRounds = 2\nstack = 1200 1200\nblind = 100 100\nfirstPlayer = 1 1\nnumSuits = 2\nnumRanks = 3\nnumHoleCards = 1\nnumBoardCards = 0 1\nEND GAMEDEF");
 
     GameTree tree(gameDesc);
 
     std::srand(std::time(nullptr));
 
 
-    for( int i = 0; i < 100; i++) {
+    for (int i = 0; i < 100; i++) {
         GameTree::GameNode node(&tree);
         std::cout << node.ToString() << std::endl;
         while (!node.IsFinished()) {
@@ -22,27 +23,41 @@ void BasicGameTreeTests() {
             uint32_t actions = node.GetActionCount();
             uint32_t action = std::rand() % actions;
 
-            std::cout << "Choose Action: " << action <<std::endl;
+            std::cout << "Choose Action: " << action << std::endl;
             node.ApplyAction(action);
             std::cout << node.ToString() << std::endl;
         }
     }
+}
+
+void HoldemGameTreeTests()
+{
+    const std::string gameDescHoldem("GAMEDEF\nnolimit\nnumPlayers = 2\nnumRounds = 4\nstack = 20000 20000\nblind = 100 50\nfirstPlayer = 2 1 1 1\nnumSuits = 4\nnumRanks = 13\nnumHoleCards = 2\nnumBoardCards = 0 3 1 1\nEND GAMEDEF");
+
+    GameTree tree(gameDescHoldem);
+
+    std::srand(std::time(nullptr));
 
 
+    for (int i = 0; i < 100; i++) {
+        GameTree::GameNode node(&tree);
+        while (!node.IsFinished()) {
+            uint32_t actions = node.GetActionCount();
+            uint32_t action = std::rand() % actions;
+            node.ApplyAction(action);
+        }
 
-
-
-
-
-
-
-
+        std::cout << node.ToString() << std::endl;
+    }
 
 }
+
+
 }  // namespace
 
 int main(int argc, char **argv) {
 
     open_spiel::universal_poker::logic::BasicGameTreeTests();
+    open_spiel::universal_poker::logic::HoldemGameTreeTests();
 
 }
