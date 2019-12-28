0c491343dcdeccf5741905edda1440d89ad0e3cd
DeepMind Technologies Ltd <open_spiel@google.com>
2019-12-27 23:51:56 -0700

    Improve checking + installing dependencies on MacOS X and add coreutils (which contains realpath needed by build_and_run_tests.sh)
    PiperOrigin-RevId: 287374357
    Change-Id: I8a47d0b78e9b81c39d4afcc03e1fff51aeeca65f

--- install.sh
+++ install.sh
@@ -106,8 +106,9 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python${OS_PYTHON_VERSION} 10
   fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then  # Mac OSX
-  [[ -x "$(type python3)" ]] || brew install python3 || echo "** Warning: failed 'brew install python3' -- continuing"
-  [[ -x "$(type g++-7)" ]] || brew install gcc@7 || echo "** Warning: failed 'brew install gcc@7' -- continuing"
+  [[ -x `which realpath` ]] || brew install coreutils || echo "** Warning: failed 'brew install coreutils' -- continuing"
+  [[ -x `which python3` ]] || brew install python3 || echo "** Warning: failed 'brew install python3' -- continuing"
+  [[ -x `which g++-7` ]] || brew install gcc@7 || echo "** Warning: failed 'brew install gcc@7' -- continuing"
   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
   python3 get-pip.py
   pip3 install virtualenv
