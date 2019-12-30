a67bf016064b02285d4d3cd72d03497635b48806
DeepMind Technologies Ltd <open_spiel@google.com>
2019-12-30 13:00:49 -0700

    Upgrade pip and setuptools as non-root in Travis.
    PiperOrigin-RevId: 287578310
    Change-Id: I1c43e62d52095470b6ae9572216dae35593c8227

--- open_spiel/scripts/travis_script.sh
+++ open_spiel/scripts/travis_script.sh
@@ -17,13 +17,13 @@
 set -e
 set -x
 
+pip3 install --upgrade pip
+pip3 install --upgrade setuptools
+
 virtualenv -p python ./venv
 source ./venv/bin/activate
 
 python --version
-
-pip3 install --upgrade pip
-pip3 install --upgrade setuptools
 pip3 install --upgrade -r requirements.txt -q
 
 ./open_spiel/scripts/build_and_run_tests.sh
