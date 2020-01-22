b47c9e2e46697e6b1192931ddd17deb47c88388b
DeepMind Technologies Ltd <open_spiel@google.com>
2020-01-22 21:43:20 +0000

    More-readable formatting of observation and information state tensors.
    PiperOrigin-RevId: 291021275
    Change-Id: Ie246bdcaca002365da8118309cc72f8d8d0f34f5

--- open_spiel/python/algorithms/generate_playthrough.py
+++ open_spiel/python/algorithms/generate_playthrough.py
@@ -41,6 +41,59 @@ def _escape(x):
   return x
 
 
+def _format_value(v):
+  """Format a single value."""
+  if v == 0:
+    return "◯"
+  elif v == 1:
+    return "◉"
+  else:
+    return ValueError("Values must all be 0 or 1")
+
+
+def _format_vec(vec):
+  return "".join(_format_value(v) for v in vec)
+
+
+def _format_matrix(mat):
+  return np.char.array([_format_vec(row) for row in mat])
+
+
+def _format_tensor(tensor, tensor_name, max_cols=120):
+  """Formats a tensor in an easy-to-view format as a list of lines."""
+  if ((tensor.shape == (0,)) or (len(tensor.shape) > 3) or
+      not np.logical_or(tensor == 0, tensor == 1).all()):
+    vec = ", ".join(str(round(v, 5)) for v in tensor.ravel())
+    return ["{} = [{}]".format(tensor_name, vec)]
+  elif len(tensor.shape) == 1:
+    return ["{}: {}".format(tensor_name, _format_vec(tensor))]
+  elif len(tensor.shape) == 2:
+    if len(tensor_name) + tensor.shape[0] + 2 < max_cols:
+      lines = ["{}: {}".format(tensor_name, _format_vec(tensor[0]))]
+      prefix = " " * (len(tensor_name) + 2)
+    else:
+      lines = ["{}:".format(tensor_name), _format_vec(tensor[0])]
+      prefix = ""
+    for row in tensor[1:]:
+      lines.append(prefix + _format_vec(row))
+    return lines
+  elif len(tensor.shape) == 3:
+    lines = ["{}:".format(tensor_name)]
+    rows = []
+    for m in tensor:
+      formatted_matrix = _format_matrix(m)
+      if (not rows) or (len(rows[-1][0] + formatted_matrix[0]) + 2 > max_cols):
+        rows.append(formatted_matrix)
+      else:
+        rows[-1] = rows[-1] + "  " + formatted_matrix
+    for i, big_row in enumerate(rows):
+      if i > 0:
+        lines.append("")
+      for row in big_row:
+        lines.append("".join(row))
+    return lines
+
+
 def playthrough(game_string, action_sequence, alsologtostdout=False):
   """Returns a playthrough of the specified game as a single text.
 
@@ -155,18 +208,22 @@ def playthrough_lines(game_string, alsologtostdout=False, action_sequence=None):
             player, _escape(state.information_state_string(player))))
     if game.get_type().provides_information_state_tensor:
       for player in players:
-        vec = ", ".join(
-            str(round(x, 5)) for x in state.information_state_tensor(player))
-        add_line("InformationStateTensor({}) = [{}]".format(player, vec))
+        label = "InformationStateTensor({})".format(player)
+        lines += _format_tensor(
+            np.reshape(
+                state.information_state_tensor(player),
+                game.information_state_tensor_shape()), label)
     if game.get_type().provides_observation_string:
       for player in players:
         add_line('ObservationString({}) = "{}"'.format(
             player, _escape(state.observation_string(player))))
     if game.get_type().provides_observation_tensor:
       for player in players:
-        vec = ", ".join(
-            str(round(x, 5)) for x in state.observation_tensor(player))
-        add_line("ObservationTensor({}) = [{}]".format(player, vec))
+        label = "ObservationTensor({})".format(player)
+        lines += _format_tensor(
+            np.reshape(
+                state.observation_tensor(player),
+                game.observation_tensor_shape()), label)
     if game_type.chance_mode == pyspiel.GameType.ChanceMode.SAMPLED_STOCHASTIC:
       add_line('SerializeState() = "{}"'.format(_escape(state.serialize())))
     if not state.is_chance_node():
