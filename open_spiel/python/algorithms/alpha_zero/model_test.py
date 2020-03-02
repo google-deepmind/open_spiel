9b58101dfa07a3e0559a351ba9c62b85105466ef
DeepMind Technologies Ltd <open_spiel@google.com>
2020-03-02 12:36:03 -0700

    Test that the model can learn the solution to the entire game of tic tac toe.
    PiperOrigin-RevId: 298403842
    Change-Id: Ifa594eac162f4126f067be2c7521b9ad62a2db56

--- open_spiel/python/algorithms/alpha_zero/model_test.py
+++ open_spiel/python/algorithms/alpha_zero/model_test.py
@@ -42,26 +42,57 @@ FLAGS = flags.FLAGS
 
 tf.enable_eager_execution()
 
+solved = {}
+
+
+def solve_game(state):
+  state_str = str(state)
+  if state_str in solved:
+    return solved[state_str].value
+  if state.is_terminal():
+    return state.returns()[0]
+
+  max_player = state.current_player() == 0
+  obs = state.observation_tensor()
+  act_mask = np.array(state.legal_actions_mask())
+  values = np.full(act_mask.shape, -2 if max_player else 2)
+  for action in state.legal_actions():
+    values[action] = solve_game(state.child(action))
+  value = values.max() if max_player else values.min()
+  best_actions = np.where((values == value) & act_mask)
+  policy = np.zeros_like(act_mask)
+  policy[best_actions[0][0]] = 1  # Choose the first for a deterministic policy.
+  solved[state_str] = model_lib.TrainInput(obs, act_mask, policy, value)
+  return value
+
+
+model_types = ["resnet", "mlp"]
+
+
+def build_model(game, model_type):
+  num_actions = game.num_distinct_actions()
+  observation_shape = game.observation_tensor_shape()
+
+  if model_type == "resnet":
+    net = model_lib.keras_resnet(
+        observation_shape, num_actions, num_residual_blocks=2, num_filters=64,
+        data_format="channels_first")
+  elif model_type == "mlp":
+    net = model_lib.keras_mlp(
+        observation_shape, num_actions, num_layers=2, num_hidden=64)
+  else:
+    raise ValueError("Invalid model_type: {}".format(model_type))
+
+  return model_lib.Model(
+      net, l2_regularization=1e-4, learning_rate=0.01, device="cpu")
+
 
 class ModelTest(parameterized.TestCase):
 
-  @parameterized.parameters(["resnet", "mlp"])
-  def test_model_learns_single(self, model_type):
+  @parameterized.parameters(model_types)
+  def test_model_learns_simple(self, model_type):
     game = pyspiel.load_game("tic_tac_toe")
-    num_actions = game.num_distinct_actions()
-    observation_shape = game.observation_tensor_shape()
-
-    if model_type == "resnet":
-      net = model_lib.keras_resnet(
-          observation_shape, num_actions, num_residual_blocks=2, num_filters=64,
-          data_format="channels_first")
-    elif model_type == "mlp":
-      net = model_lib.keras_mlp(
-          observation_shape, num_actions, num_layers=2, num_hidden=64)
-    else:
-      raise ValueError("Invalid model_type: {}".format(model_type))
-    model = model_lib.Model(
-        net, l2_regularization=1e-4, learning_rate=0.01, device="cpu")
+    model = build_model(game, model_type)
 
     train_inputs = []
     state = game.new_initial_state()
@@ -84,11 +115,33 @@ class ModelTest(parameterized.TestCase):
       loss = model.update(train_inputs)
       print(i, loss)
       losses.append(loss)
-      if loss.policy + loss.value < 0.1:
+      if loss.policy < 0.05 and loss.value < 0.05:
         break
     self.assertGreater(losses[0].total, losses[-1].total)
     self.assertGreater(losses[0].policy, losses[-1].policy)
     self.assertGreater(losses[0].value, losses[-1].value)
+    self.assertLess(losses[-1].value, 0.05)
+
+  @parameterized.parameters(model_types)
+  def test_model_learns_optimal(self, model_type):
+    game = pyspiel.load_game("tic_tac_toe")
+    solve_game(game.new_initial_state())
+
+    model = build_model(game, model_type)
+
+    train_inputs = list(solved.values())
+    print("states:", len(train_inputs))
+    losses = []
+    for i in range(2000):
+      loss = model.update(train_inputs)
+      print(i, loss)
+      losses.append(loss)
+      if loss.policy < 0.1 and loss.value < 0.1:
+        break
+
+    self.assertGreater(losses[0].policy, losses[-1].policy)
+    self.assertGreater(losses[0].value, losses[-1].value)
+    self.assertGreater(losses[0].total, losses[-1].total)
     self.assertLess(losses[-1].value, 0.1)
 
 
