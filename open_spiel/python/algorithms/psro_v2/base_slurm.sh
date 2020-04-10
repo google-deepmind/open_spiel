758054ace9673f532552a7f51b38d41d8a21cb0a
Yongzhao <wangyzh@umich.edu>
2020-04-10 16:54:57 -0400

    "add slurm scripts generator and batch submission bash file."

--- open_spiel/python/algorithms/psro_v2/base_slurm.sh
+++ open_spiel/python/algorithms/psro_v2/base_slurm.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=5g
-#SBATCH --time=02-00:00:00
+#SBATCH --time=04-00:00:00
 #SBATCH --account=wellman1
 #SBATCH --partition=standard
 
