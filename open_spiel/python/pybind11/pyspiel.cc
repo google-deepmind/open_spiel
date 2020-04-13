f0da7a81bdbcfad4dd2658ed66349e42e53ec741
DeepMind Technologies Ltd <open_spiel@google.com>
2020-04-12 20:59:29 -0600

    Refactor and improve support for Gambit .efg format (determine unique actions using their labels, move example data strings to their own file, and add another example game).
    PiperOrigin-RevId: 306167177
    Change-Id: I3547d7ca72a70daffbc59b8a3e058e55007e678e

--- open_spiel/python/pybind11/pyspiel.cc
+++ open_spiel/python/pybind11/pyspiel.cc
@@ -31,6 +31,7 @@
 #include "open_spiel/game_transforms/normal_form_extensive_game.h"
 #include "open_spiel/game_transforms/turn_based_simultaneous_game.h"
 #include "open_spiel/games/efg_game.h"
+#include "open_spiel/games/efg_game_data.h"
 #include "open_spiel/matrix_game.h"
 #include "open_spiel/normal_form_game.h"
 #include "open_spiel/policy.h"
