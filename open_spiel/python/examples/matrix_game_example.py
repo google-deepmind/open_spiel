77c4d4fd927f156a503e11c3f913c40f53268516
DeepMind Technologies Ltd <open_spiel@google.com>
2020-04-30 03:45:17 -0600

    Adds example of creating a matrix game via imported payoffs.
    PiperOrigin-RevId: 309189908
    Change-Id: I4e877fd041fcdb38942488f7ddc610a27968d2ba

--- open_spiel/python/examples/matrix_game_example.py
+++ open_spiel/python/examples/matrix_game_example.py
@@ -19,9 +19,12 @@ from __future__ import division
 from __future__ import print_function
 
 import random
+
 from absl import app
+import numpy as np
 
 import pyspiel
+from open_spiel.python.utils import file_utils
 
 
 def _manually_create_game():
@@ -65,6 +68,14 @@ def _even_easier_create_game():
   return pyspiel.create_matrix_game([[-1, 1], [1, -1]], [[1, -1], [-1, 1]])
 
 
+def _import_data_create_game():
+  """Creates a game via imported payoff data."""
+  payoff_file = file_utils.find_file(
+      "open_spiel/data/paper_data/response_graph_ucb/soccer.txt", 2)
+  payoffs = np.loadtxt(payoff_file)*2-1
+  return pyspiel.create_matrix_game(payoffs, payoffs.T)
+
+
 def main(_):
   games_list = pyspiel.registered_games()
   print("Registered games:")
@@ -79,6 +90,7 @@ def main(_):
   print("Creating matrix game...")
   game = pyspiel.load_matrix_game("matrix_mp")
   game = _manually_create_game()
+  game = _import_data_create_game()
   game = _easy_create_game()
   game = _even_easier_create_game()
 
