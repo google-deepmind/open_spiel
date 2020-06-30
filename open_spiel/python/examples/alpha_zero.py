e80f6be36178057c29da39de205266fdbd81b1a9
BedirT <tapkan@ualberta.ca>
2020-06-29 20:41:04 -0600

    minimax az

--- open_spiel/python/examples/alpha_zero.py
+++ open_spiel/python/examples/alpha_zero.py
@@ -18,13 +18,13 @@
 from absl import app
 from absl import flags
 
-from open_spiel.python.algorithms.alpha_zero import alpha_zero
-from open_spiel.python.algorithms.alpha_zero import model as model_lib
+from open_spiel.python.algorithms.alpha_zero_mm import alpha_zero
+from open_spiel.python.algorithms.alpha_zero_mm import model as model_lib
 from open_spiel.python.utils import spawn
 
-flags.DEFINE_string("game", "connect_four", "Name of the game.")
+flags.DEFINE_string("game", "hex", "Name of the game.")
 flags.DEFINE_integer("uct_c", 2, "UCT's exploration constant.")
-flags.DEFINE_integer("max_simulations", 300, "How many simulations to run.")
+flags.DEFINE_integer("max_simulations", 100, "How many simulations to run.")
 flags.DEFINE_integer("train_batch_size", 2 ** 10, "Batch size for learning.")
 flags.DEFINE_integer("replay_buffer_size", 2 ** 16,
                      "How many states to store in the replay buffer.")
@@ -40,10 +40,10 @@ flags.DEFINE_integer("temperature_drop", 10,  # Less than AZ due to short games.
                      "Drop the temperature to 0 after this many moves.")
 flags.DEFINE_enum("nn_model", "resnet", model_lib.Model.valid_model_types,
                   "What type of model should be used?.")
-flags.DEFINE_integer("nn_width", 2 ** 7, "How wide should the network be.")
+flags.DEFINE_integer("nn_width", 2 ** 6, "How wide should the network be.")
 flags.DEFINE_integer("nn_depth", 10, "How deep should the network be.")
 flags.DEFINE_string("path", None, "Where to save checkpoints.")
-flags.DEFINE_integer("checkpoint_freq", 100, "Save a checkpoint every N steps.")
+flags.DEFINE_integer("checkpoint_freq", 10, "Save a checkpoint every N steps.")
 flags.DEFINE_integer("actors", 2, "How many actors to run.")
 flags.DEFINE_integer("evaluators", 1, "How many evaluators to run.")
 flags.DEFINE_integer("evaluation_window", 100,
@@ -53,8 +53,8 @@ flags.DEFINE_integer(
     ("Play evaluation games vs MCTS+Solver, with max_simulations*10^(n/2)"
      " simulations for n in range(eval_levels). Default of 7 means "
      "running mcts with up to 1000 times more simulations."))
-flags.DEFINE_integer("max_steps", 0, "How many learn steps before exiting.")
-flags.DEFINE_bool("quiet", True, "Don't show the moves as they're played.")
+flags.DEFINE_integer("max_steps", 20, "How many learn steps before exiting.")
+flags.DEFINE_bool("quiet", False, "Don't show the moves as they're played.")
 flags.DEFINE_bool("verbose", False, "Show the MCTS stats of possible moves.")
 
 FLAGS = flags.FLAGS
