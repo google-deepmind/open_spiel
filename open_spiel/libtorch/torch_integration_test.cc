f33660aaabb468d6847abb7f36d31b0251e6e167
Michal Sustr <michal.sustr@aic.fel.cvut.cz>
2020-09-16 16:52:05 +0200

    Fix includes in integration test example.

--- open_spiel/libtorch/torch_integration_test.cc
+++ open_spiel/libtorch/torch_integration_test.cc
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "//third_party/open_spiel/spiel_utils.h"
-#include "<torch/torch.h>"
+#include "open_spiel/spiel_utils.h"
+#include "torch/torch.h"
 
 namespace {
 
