5f17cc96170daf185c40ee8eeeaad90998f92859
Gabriele Farina <gfarina@cs.cmu.edu>
2020-09-25 01:59:14 -0400

    Add or update copyright notices everywhere

--- open_spiel/games/battleship.h
+++ open_spiel/games/battleship.h
@@ -1,4 +1,4 @@
-// Copyright 2019 DeepMind Technologies Ltd. All rights reserved.
+// Copyright 2020 DeepMind Technologies Ltd. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
