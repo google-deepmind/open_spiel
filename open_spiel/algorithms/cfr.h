8471ad0a13b2b40bb98dd5d6082398da31242f23
Michal Sustr <michal.sustr@aic.fel.cvut.cz>
2020-10-16 12:36:18 +0200

    Make sure that GetStatePolicy primarily uses (State, Player) variant.

--- open_spiel/algorithms/cfr.h
+++ open_spiel/algorithms/cfr.h
@@ -117,7 +117,11 @@ class CFRAveragePolicy : public Policy {
   // return a uniform policy.
   CFRAveragePolicy(const CFRInfoStateValuesTable& info_states,
                    std::shared_ptr<Policy> default_policy);
-  ActionsAndProbs GetStatePolicy(const State& state) const override;
+  ActionsAndProbs GetStatePolicy(const State& state) const override {
+    return GetStatePolicy(state, state.CurrentPlayer());
+  };
+  ActionsAndProbs GetStatePolicy(const State& state,
+                                 Player player) const override;
   ActionsAndProbs GetStatePolicy(const std::string& info_state) const override;
 
  private:
@@ -137,7 +141,11 @@ class CFRCurrentPolicy : public Policy {
   // to not use a default policy).
   CFRCurrentPolicy(const CFRInfoStateValuesTable& info_states,
                    std::shared_ptr<Policy> default_policy);
-  ActionsAndProbs GetStatePolicy(const State& state) const override;
+  ActionsAndProbs GetStatePolicy(const State& state) const override {
+    return GetStatePolicy(state, state.CurrentPlayer());
+  };
+  ActionsAndProbs GetStatePolicy(const State& state,
+                                 Player player) const override;
   ActionsAndProbs GetStatePolicy(const std::string& info_state) const override;
   TabularPolicy AsTabular() const;
 
