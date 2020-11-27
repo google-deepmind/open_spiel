54e7ab22c9b87ce950c09f16284f9ec873426b32
lanctot <lanctot@google.com>
2020-11-26 22:10:00 -0330

    noxfile: explicitly forward CC & CXX env vars

--- noxfile.py
+++ noxfile.py
@@ -29,9 +29,17 @@ def get_distutils_tempdir():
 
 @nox.session(python="3")
 def tests(session):
+  # Explicitly pull out CC and CXX if they are specified.
+  child_env = {}  
+  if os.environ.get("CC") is not None:
+    child_env["CC"] = os.environ.get("CC")
+  if os.environ.get("CXX") is not None:
+    child_env["CXX"] = os.environ.get("CXX")
+  print("nox build child environment:")
+  print(child_env)
   session.install("-r", "requirements.txt")
-  session.run("python3", "setup.py", "build")
-  session.run("python3", "setup.py", "install")
+  session.run("python3", "setup.py", "build", env=child_env)
+  session.run("python3", "setup.py", "install", env=child_env)
   session.cd(os.path.join("build", get_distutils_tempdir()))
   session.run(
       "ctest", f"-j{4*os.cpu_count()}", "--output-on-failure", external=True)
