738cabe51436dc62901e1e30e8171d81ae7646f3
Marc Lanctot <marc.lanctot@gmail.com>
2020-12-02 12:55:51 -0330

    Added back licenses and made C++ error more specific

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+# Copyright 2019 DeepMind Technologies Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # Lint as: python3
 """The setup script for setuptools.
 
@@ -54,8 +68,9 @@ class BuildExt(build_ext):
     except OSError as e:
       ext_names = ", ".join(e.name for e in self.extensions)
       raise RuntimeError(
-          "{} must be installed to build the following extensions: {}".format(
-              cxx, ext_names)
+          "A C++ compiler that supports c++17 must be installed to build the "
+          + "following extensions: {}".format(ext_names)
+          + ". We recommend: Clang version >= 7.0.0."
       ) from e
     print("Found C++ compiler: {}".format(cxx))
 
