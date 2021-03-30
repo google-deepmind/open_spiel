d1530556d2ae50301df99b78e0367f007e2aaf65
BedirT <tapkan@ualberta.ca>
2021-03-30 16:27:17 -0600

    changed syntax

--- open_spiel/games/hex.cc
+++ open_spiel/games/hex.cc
@@ -217,19 +217,9 @@ std::string HexState::ActionToString(Player player, Action action_id) const {
 std::vector<int> HexState::AdjacentCells(int cell) const {
   std::vector<int> neighbours = {};
   if (board_size_ == 2) {
-    switch(cell) {
-      case 0:
-        neighbours = {1, 2};
-        break;
-      case 1:
-        neighbours = {0, 2, 3};
-        break;
-      case 2:
-        neighbours = {0, 1, 3};
-        break;
-      case 3:
-        neighbours = {1, 2};
-        break;
+    if cell == 0 || cell == 3) neighbours = {1, 2};
+    else if (cell == 1) neighbours = {0, 2, 3};
+    else if (cell == 2) neighbours = {0, 1, 3};
     }
   }
   else {
