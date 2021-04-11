c970f230afb3455cc3adc69c08579455253d1948
Christian Jans <cjjans@ualberta.ca>
2021-04-11 10:23:01 -0600

    Print legal actions in columns

--- open_spiel/bots/human/human_bot.cc
+++ open_spiel/bots/human/human_bot.cc
@@ -14,15 +14,43 @@
 
 #include "open_spiel/bots/human/human_bot.h"
 
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
 #include <iostream>
+#include <string>
 #include <unordered_map>
+#include <utility>
 
 namespace open_spiel {
 namespace {
 
-// TODO: Perhaps use this function to print the legal actions nicely.
-void PrintColumns() {
-  
+const int kMaxWidth = 80;
+const int kPadding = 2;
+
+void PrintColumns(const std::vector<std::string>& strings) {
+  std::string padding_string(kPadding, ' ');
+
+  int longest_string_length = 0;
+  for (const std::string& string : strings) {
+    if (string.length() > longest_string_length) {
+      longest_string_length = string.length();
+    }
+  }
+
+  int max_columns = (kMaxWidth - 1) / (longest_string_length + 2 * kPadding);
+  int rows = ceil((float) strings.size() / (float) max_columns);
+  int columns = ceil((float) strings.size() / (float) rows);
+  for (int row = 0; row < rows; ++row) {
+    for (int column = 0; column < columns; ++column) {
+      int index = row + column * rows;
+      if (index < strings.size()) {
+        std::cout << std::left << std::setw(longest_string_length + kPadding)
+                  << padding_string << strings[index];
+      }
+    }
+    std::cout << "\n";
+  }
 }
 
 }  // namespace
@@ -46,21 +74,47 @@ Action HumanBot::Step(const State& state) {
     std::cout << "Choose an action (empty to print legal actions): ";
     std::getline(std::cin, action_string);
 
+    // Print the legal actions if no action is given.
     if (action_string.empty()) {
       std::cout << "Legal action(s):\n";
-      for (Action legal_action : legal_actions) {
-        // TODO: Clean up and make it print nicer.
-        std::cout << legal_action << ": " << state.ActionToString(legal_action)
-            << " ";
+
+      std::vector<std::string> legal_action_strings;
+      std::vector<std::pair<std::string, Action>> sorted_action_map(
+          action_map.begin(), action_map.end());
+
+      std::sort(sorted_action_map.begin(),
+                sorted_action_map.end(),
+                [](const auto& left, const auto& right) {
+                  return left.first.compare(right.first);
+                });
+
+      int longest_action_length = 0;
+      for (const Action& legal_action : legal_actions) {
+        int action_length = std::to_string(legal_action).length();
+        if (action_length > longest_action_length) {
+          longest_action_length = action_length;
+        }
       }
-      std::cout << "\n";
+
+      for (const auto& string_action_pair : sorted_action_map) {
+        std::string action_string = string_action_pair.first;
+        std::string action_int_string =
+            std::to_string(string_action_pair.second);
+        std::string action_padding(
+            longest_action_length - action_int_string.length(), ' ');
+        legal_action_strings.push_back(absl::StrCat(
+            action_padding, action_int_string, ": ", action_string));
+      }
+      PrintColumns(legal_action_strings);
       continue;
     }
 
+    // Return the action if a valid string is given.
     if (action_map.find(action_string) != action_map.end()) {
       return action_map[action_string];
     }
 
+    // Return the action if a valid integer is given.
     try {
       action = std::stoi(action_string);
     } catch (const std::exception& e) {
@@ -74,6 +128,7 @@ Action HumanBot::Step(const State& state) {
       }
     }
 
+    // The input was not valid.
     std::cout << "Illegal action selected: " << action_string << "\n";
   }
 }
