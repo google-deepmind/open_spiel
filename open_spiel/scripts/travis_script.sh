abce0a101535cc6fa68ba48566e5ebe9cae3cdf5
lanctot <lanctot@google.com>
2021-04-19 13:46:07 -0230

    Unescape '>' and '<'

--- open_spiel/scripts/travis_script.sh
+++ open_spiel/scripts/travis_script.sh
@@ -19,7 +19,7 @@ set -x
 
 PYTHON_JAX_DEPS="jax==0.2.7 jaxlib==0.1.57 dm-haiku==0.0.3 optax==0.0.2 chex==0.0.3"
 PYTHON_PYTORCH_DEPS="torch==1.7.0"
-PYTHON_TENSORFLOW_DEPS="tensorflow==2.4.1 tensorflow-probability\<0.8.0,\>=0.7.0"
+PYTHON_TENSORFLOW_DEPS="tensorflow==2.4.1 tensorflow-probability<0.8.0,>=0.7.0"
 
 if [ ! $TRAVIS_USE_NOX -eq 0 ]; then
   # Build and run tests using nox
