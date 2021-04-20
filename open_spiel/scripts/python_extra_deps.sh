4c79d684040868a749c9ecb7caef13224a96344c
Marc Lanctot <marc.lanctot@gmail.com>
2021-04-19 23:07:56 -0230

    Re-add last missing dependency

--- open_spiel/scripts/python_extra_deps.sh
+++ open_spiel/scripts/python_extra_deps.sh
@@ -2,4 +2,4 @@
 export OPEN_SPIEL_PYTHON_JAX_DEPS="jax==0.2.7 jaxlib==0.1.57 dm-haiku==0.0.3 optax==0.0.2 chex==0.0.3"
 export OPEN_SPIEL_PYTHON_PYTORCH_DEPS="torch==1.7.0"
 export OPEN_SPIEL_PYTHON_TENSORFLOW_DEPS="tensorflow==2.4.1 tensorflow-probability<0.8.0,>=0.7.0"
-export OPEN_SPIEL_PYTHON_MISC_DEPS="IPython==5.8.0 cvxopt==1.2.5 networkx==2.4 matplotlib==3.2.2 nashpy==0.0.19 scipy==1.4.1 testresources==2.0.1"
+export OPEN_SPIEL_PYTHON_MISC_DEPS="IPython==5.8.0 cvxopt==1.2.5 networkx==2.4 matplotlib==3.2.2 mock==4.0.2 nashpy==0.0.19 scipy==1.4.1 testresources==2.0.1"
