23dffca8084146fd84b4d99d2b20f0b2b15f29b4
Theophile Cabannes <theophile@berkeley.edu>
2021-05-12 12:51:48 +0200

    Implement dummy dynamic routing game within OpenSpiel

---
+++ open_spiel/python/games/dynamic_routing_game.py
@@ -0,0 +1,287 @@
+# Copyright 2019 DeepMind Technologies Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as python3
+"""Simple implementation of dynamic routing game to test API.
+
+The dynamic routing game is based on the paper: A mean field route choice game
+by R. Salhab, J. Le Ny and R. P. Malhamé, 2018 IEEE CDC.
+We consider:
+- the following network:
+    O --> A --> D
+- two vehicles trying to reach D from O.
+The two vehicles leave the origin O at the beginning of the game.
+At O, they can only go at A. Then at A they can only go at D. At D, both
+vehicles cannot do any actions and the game is finished.
+
+Later, to add some complexity, we will add a third vehicle who leaves A to
+reach D at the beginning of the game. In this case, at time O, vehicles 0 and 1
+go from O to A and vehicle 2 goes from A to D. Then at time 1, vehicles 0 and 1
+go from A to D, vehicle 2 cannot do anything and the game is finished.
+
+Later a more complex network can considered, with routing choice at
+intersections and probability to stay on a link as a function of the number of
+cars on the link. Currently this is not done to test the API.
+"""
+
+import numpy as np
+from typing import Dict, Iterable, List, NewType, Set, Tuple
+
+import pyspiel
+
+RoadSection = NewType("RoadSection", str)
+
+_GAME_TYPE = pyspiel.GameType(
+    short_name="python_markov_dynamic_routing_game",
+    long_name="Python Markov Dynamic Routing Game",
+    dynamics=pyspiel.GameType.Dynamics.SIMULTANEOUS,
+    chance_mode=pyspiel.GameType.ChanceMode.DETERMINISTIC,
+    information=pyspiel.GameType.Information.PERFECT_INFORMATION,
+    utility=pyspiel.GameType.Utility.GENERAL_SUM,
+    reward_model=pyspiel.GameType.RewardModel.TERMINAL,
+    max_num_players=3,
+    min_num_players=0,
+    provides_information_state_string=True,
+    provides_information_state_tensor=True,
+    provides_observation_string=True,
+    provides_observation_tensor=True,
+    parameter_specification={})
+_NETWORK_ADJACENCY_LIST = {"O": ["A"], "A": ["D"]}
+
+
+# movement to actions and action to road section can be created from
+# a function.
+def _movement_to_string(origin: RoadSection, destination: RoadSection) -> str:
+    return f"{origin}->{destination}"
+
+
+def _create_movement_to_action_and_action_to_road_section(
+    network_adjacency_list: Dict[RoadSection, Iterable[RoadSection]]
+) -> Tuple[Dict[str, int], Dict[int, RoadSection]]:
+    """Create dictionary that maps movement to action.
+
+    The dictionary that maps movement to action is used to define the action
+    from a movement that a vehicle would like to do. The dictionary that maps
+    an action to the destintion of the movement is used to move a vehicle that
+    does an action to the destination of its movement.
+    Args:
+        network_adjacency_list: adjacency list of the network.
+    Returns:
+        movement_to_action: dictionary with key begin a movement for example
+            "O->A" and value the action numbers. Action numbers are succesive
+            integers indexed from 0.
+        action_to_road_section: map an action number to the end node of the
+            movement. if movement_to_action["O->A"] = 0 then,
+            action_to_road_section[0] = "A"
+    """
+    movement_to_action = {}
+    action_to_road_section = {}
+    action_number = 0
+    for origin, successors in network_adjacency_list.items():
+        for destination in successors:
+            movement_to_action[_movement_to_string(origin, destination)] =\
+                action_number
+            action_to_road_section[action_number] = destination
+            action_number += 1
+    return movement_to_action, action_to_road_section
+
+
+_MOVEMENT_TO_ACTION, _ACTION_TO_ROAD_SECTION =\
+    _create_movement_to_action_and_action_to_road_section(
+        _NETWORK_ADJACENCY_LIST)
+
+
+class DynamicRoutingGame(pyspiel.Game):
+    """TODO(theo): write docstring.
+    """
+    def __init__(self, params=None, num_players: int = 2):
+        max_number_time_step = 2
+        game_info = pyspiel.GameInfo(
+            num_distinct_actions=len(_ACTION_TO_ROAD_SECTION),
+            max_chance_outcomes=0,
+            num_players=num_players,
+            min_utility=-max_number_time_step-1,
+            max_utility=0,
+            max_game_length=max_number_time_step)
+        super().__init__(_GAME_TYPE, game_info, dict())
+
+    def new_initial_state(self) -> "DynamicRoutingGame":
+        """Returns a state corresponding to the start of a game."""
+        return DynamicRoutingGame(self)
+
+    def make_py_observer(self, iig_obs_type=None, params=None):
+        """Returns an object used for observing game state."""
+        return NetworkObserver()
+
+
+class DynamicRoutingGameState(pyspiel.State):
+    """TODO(theo): write docstring.
+
+    Attributes:
+      _current_time_step:
+      _is_terminal:
+      _max_time_step:
+      _max_travel_time:
+      _num_distinct_actions:
+      _num_players:
+      _vehicle_at_destination:
+      _vehicle_destinations:
+      _vehicle_final_travel_times:
+      _vehicle_locations:
+    """
+    _current_time_step: int
+    _is_terminal: bool
+    _max_time_step: int
+    _max_travel_time: float
+    _num_distinct_actions: int
+    _num_players: int
+    _vehicle_at_destination: Set[int]
+    _vehicle_destinations: List[RoadSection]
+    _vehicle_final_travel_times: List[float]
+    _vehicle_locations: List[RoadSection]
+
+    def __init__(self, game: DynamicRoutingGame):
+        """Constructor; should only be called by Game.new_initial_state."""
+        super().__init__(game)
+        self._current_time_step = 0
+        self._is_terminal = False
+        self._max_time_step = game.max_game_length()
+        self._max_travel_time = - game.min_utility()
+        self._num_distinct_actions = game.num_distinct_actions()
+        self._num_players = game.num_players()
+        self._vehicle_at_destination = set()
+        self._vehicle_destinations = ["D" for _ in self._num_players]
+        self._vehicle_final_travel_times = [0.0 for _ in self._num_players]
+        if self._num_players == 2:
+            self._vehicle_locations = ["O", "O"]
+        elif self._num_players == 3:
+            self._vehicle_locations = ["O", "O", "A"]
+        else:
+            raise ValueError("The game should have 2 or 3 players.")
+
+    def current_player(self) -> pyspiel.PlayerId:
+        """Returns SIMULTANEOUS if the game is not over, otherwise TERMINAL."""
+        return (pyspiel.PlayerId.TERMINAL if self._is_terminal
+                else pyspiel.PlayerId.SIMULTANEOUS)
+
+    def assert_valid_action(self, action: int, location: str = ""):
+        assert isinstance(action, int)
+        assert action >= 0
+        assert action < self._num_distinct_actions
+        if location:
+            successors = _NETWORK_ADJACENCY_LIST[location]
+        assert action in successors, (
+              f"Invalid action {action}. It is not a successors of "
+              f"{location}: {successors}.")
+
+    def assert_valid_player(self, player: int):
+        assert isinstance(player, int)
+        assert player >= 0
+        assert player < self._num_players
+
+    def legal_actions(self, player: int = None) -> List[int]:
+        """TODO(Theo): write docstring.
+        """
+        if self._is_terminal:
+            return []
+        self.assert_valid_player(player)
+        if player in self._vehicle_at_destination:
+            # If the vehicle is at destination it cannot do anything.
+            return [pyspiel.INVALID_ACTION]
+        location = self._vehicle_locations[player]
+        successors = _NETWORK_ADJACENCY_LIST[location]
+        if successors:
+            assert isinstance(successors, Iterable)
+            assert all([self.assert_valid_action(s) for s in successors])
+            return sorted([_MOVEMENT_TO_ACTION[_movement_to_string(location,
+                                                                   successor)]
+                           for successor in successors])
+        return [pyspiel.INVALID_ACTION]
+
+    def _apply_actions(self, actions: List[int]):
+        """TODO(Theo): Applies the specified action to the state."""
+        assert len(actions) == self._num_players, (
+          f"Each player does not have an actions. Actions has {len(actions)} "
+          f"elements, it should have {self._num_players}.")
+        for vehicle_id, action in enumerate(actions):
+            # Has the vehicle already reached its destination?
+            if vehicle_id in self._vehicle_at_destination:
+                continue
+            location = self._vehicle_locations[vehicle_id]
+            # Has the vehicle just reached its destination?
+            if (location == self._vehicle_destinations[vehicle_id]):
+                self._vehicle_final_travel_times[vehicle_id] =\
+                  self._current_time_step
+                self._vehicle_at_destination.add(vehicle_id)
+            if action != pyspiel.INVALID_ACTION:
+                self.assert_valid_action(action, location)
+                self._vehicle_locations[vehicle_id] =\
+                    _ACTION_TO_ROAD_SECTION[action]
+                # TODO(Theo): Enable mix strategies.
+        self._current_time_step += 1
+        # Is the game finished?
+        if (self._current_time_step >= self._max_time_step or
+                all(map(lambda a: a == pyspiel.INVALID_ACTION, actions))):
+            self._is_terminal = True
+            for vehicle_id in range(self._num_players):
+                if vehicle_id not in self._vehicle_at_destination:
+                    self._vehicle_final_travel_times[vehicle_id] =\
+                      self._max_travel_time
+        elif len(self._vehicle_at_destination) == self._num_players:
+            self._is_terminal = True
+
+    def _action_to_string(self, player, action) -> str:
+        """Action -> string."""
+        self.assert_valid_player(player)
+        if action == pyspiel.INVALID_ACTION:
+            return f"Vehicle {player} reach a sink node or its destination."
+        self.assert_valid_action(action)
+        return (f"Vehicle {player} would like to move to "
+                f"{_ACTION_TO_ROAD_SECTION[action]}.")
+
+    def is_terminal(self) -> bool:
+        """Returns True if the game is over."""
+        return self._is_terminal
+
+    def returns(self) -> List[float]:
+        """Total reward for each player over the course of the game so far."""
+        return [- travel_time
+                for travel_time in self._vehicle_final_travel_times]
+
+    def __str__(self) -> str:
+        """String for debug purposes. No particular semantics are required."""
+        return (f"Vehicle locations: {self._vehicle_locations}, "
+                f"time: {self._current_time_step}.")
+
+
+class NetworkObserver:
+    """Dummy observer for algorithm to work."""
+
+    def __init__(self, shape: int = 1):
+        """TODO(Theo): docstring."""
+        self.tensor = np.zeros(np.prod(shape), np.float32)
+        self.dict = {"observation": np.reshape(self.tensor, shape)}
+
+    def set_from(self, state, player):
+        """TODO(Theo): docstring."""
+        obs = self.dict["observation"]
+        obs.fill(0)
+
+    def string_from(self, state, player):
+        """TODO(Theo): docstring."""
+        return ""
+
+
+# Register the game with the OpenSpiel library
+pyspiel.register_game(_GAME_TYPE, DynamicRoutingGame)
