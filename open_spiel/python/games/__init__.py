326baab9caf3535d1973c9a4c83291f9ef993046
Edward Lockhart <locked@google.com>
2021-05-13 03:23:48 -0600

    Example of a simultaneous move game implemented in Python.
    PiperOrigin-RevId: 373540435
    Change-Id: If506cfa00e976984bd51f47806dca9fb8023d0d5

--- open_spiel/python/games/__init__.py
+++ open_spiel/python/games/__init__.py
@@ -26,5 +26,6 @@ pyspiel.register_game(_GAME_TYPE, KuhnPokerGame)
 ```
 """
 
+from open_spiel.python.games import iterated_prisoners_dilemma
 from open_spiel.python.games import kuhn_poker
 from open_spiel.python.games import tic_tac_toe
