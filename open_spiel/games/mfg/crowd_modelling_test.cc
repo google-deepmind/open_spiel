fb9ea56cbef210863566748743f87064bfc8f583
Marc Lanctot <lanctot@google.com>
2021-05-24 03:49:54 -0600

    Small typo fix to README.
    PiperOrigin-RevId: 375440970
    Change-Id: Id417447996b1005bd7a078b6ecf3614a9c1bfb5f

---
+++ open_spiel/games/mfg/crowd_modelling_test.cc
@@ -0,0 +1,142 @@
+// Copyright 2019 DeepMind Technologies Ltd. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "open_spiel/games/mfg/crowd_modelling.h"
+
+#include <random>
+#include <string>
+#include <vector>
+
+#include "open_spiel/abseil-cpp/absl/random/uniform_int_distribution.h"
+#include "open_spiel/spiel.h"
+#include "open_spiel/tests/basic_tests.h"
+
+namespace open_spiel {
+namespace crowd_modelling {
+namespace {
+
+namespace testing = open_spiel::testing;
+
+void TestLoad() {
+  testing::LoadGameTest("mfg_crowd_modelling");
+  auto game = LoadGame("mfg_crowd_modelling");
+  SPIEL_CHECK_EQ(game->GetType().dynamics, GameType::Dynamics::kMeanField);
+  auto state = game->NewInitialState();
+  auto cloned = state->Clone();
+  SPIEL_CHECK_EQ(state->ToString(), cloned->ToString());
+  testing::ChanceOutcomesTest(*game);
+}
+
+void TestLoadWithParams() {
+  auto game = LoadGame("mfg_crowd_modelling(size=100,horizon=1000)");
+  auto state = game->NewInitialState();
+  SPIEL_CHECK_EQ(game->ObservationTensorShape()[0], 1000 + 100);
+}
+
+std::vector<double> RandomDistribution(int num_states, std::mt19937& rng) {
+  std::uniform_real_distribution<double> rand(0, 1);
+  std::vector<double> distrib;
+  for (int i = 0; i < num_states; ++i) {
+    distrib.push_back(rand(rng));
+  }
+  double sum = std::accumulate(distrib.begin(), distrib.end(), 0.);
+  for (int i = 0; i < num_states; ++i) {
+    distrib[i] /= sum;
+  }
+  return distrib;
+}
+
+void CheckStatesEqual(const State& a, const State& b) {
+  const CrowdModellingState& left =
+      open_spiel::down_cast<const CrowdModellingState&>(a);
+  const CrowdModellingState& right =
+      open_spiel::down_cast<const CrowdModellingState&>(b);
+  SPIEL_CHECK_EQ(left.ToString(), right.ToString());
+  SPIEL_CHECK_FLOAT_EQ(left.Rewards()[0], right.Rewards()[0]);
+  SPIEL_CHECK_FLOAT_EQ(left.Returns()[0], right.Returns()[0]);
+  SPIEL_CHECK_EQ(left.CurrentPlayer(), right.CurrentPlayer());
+  auto left_distrib = left.Distribution();
+  auto right_distrib = right.Distribution();
+  SPIEL_CHECK_EQ(left_distrib.size(), right_distrib.size());
+  for (int i = 0; i < left_distrib.size(); ++i) {
+    SPIEL_CHECK_FLOAT_EQ(left_distrib[i], right_distrib[i]);
+  }
+}
+
+void TestRandomPlay() {
+  std::mt19937 rng(7);
+  // TODO(author15): Should we adapt and use testing::RandomSimTest instead?
+  auto game = LoadGame("mfg_crowd_modelling(size=10,horizon=20)");
+  auto state = game->NewInitialState();
+  int t = 0;
+  int num_moves = 0;
+  while (!state->IsTerminal()) {
+    SPIEL_CHECK_LT(state->MoveNumber(), game->MaxMoveNumber());
+    SPIEL_CHECK_EQ(state->MoveNumber(), num_moves);
+    CheckStatesEqual(*game->DeserializeState(state->Serialize()), *state);
+    auto cloned = state->Clone();
+    CheckStatesEqual(*state, *cloned);
+    testing::CheckLegalActionsAreSorted(*game, *state);
+    if (state->CurrentPlayer() == kChancePlayerId) {
+      ActionsAndProbs outcomes = state->ChanceOutcomes();
+      Action action = open_spiel::SampleAction(outcomes, rng).first;
+      SPIEL_CHECK_EQ(state->LegalActions().size(), outcomes.size());
+      state->ApplyAction(action);
+      ++num_moves;
+    } else if (state->CurrentPlayer() == kMeanFieldPlayerId) {
+      auto support = state->DistributionSupport();
+      state->UpdateDistribution(RandomDistribution(support.size(), rng));
+    } else {
+      SPIEL_CHECK_EQ(state->CurrentPlayer(), 0);
+      std::vector<Action> actions = state->LegalActions();
+      std::uniform_int_distribution<int> dis(0, actions.size() - 1);
+      Action action = actions[dis(rng)];
+      state->ApplyAction(action);
+      ++t;
+      ++num_moves;
+    }
+  }
+  SPIEL_CHECK_EQ(t, 20);
+  SPIEL_CHECK_EQ(state->MoveNumber(), game->MaxMoveNumber());
+  SPIEL_CHECK_EQ(state->MoveNumber(), num_moves);
+}
+
+void TestReward() {
+  auto game = LoadGame("mfg_crowd_modelling(size=10,horizon=20)");
+  auto state = game->NewInitialState();
+  SPIEL_CHECK_EQ(state->CurrentPlayer(), kChancePlayerId);
+  state->ApplyAction(5);
+  SPIEL_CHECK_EQ(state->CurrentPlayer(), 0);
+  // This expected reward assumes that the game is initialized with
+  // a uniform state distribution.
+  SPIEL_CHECK_FLOAT_EQ(state->Rewards()[0], 1. + std::log(10));
+  SPIEL_CHECK_FLOAT_EQ(state->Returns()[0], 1. + std::log(10));
+
+  state->ApplyAction(1);
+  SPIEL_CHECK_EQ(state->CurrentPlayer(), kMeanFieldPlayerId);
+  SPIEL_CHECK_FLOAT_EQ(state->CurrentPlayer(), kMeanFieldPlayerId);
+  SPIEL_CHECK_FLOAT_EQ(state->Rewards()[0], 0.);
+  SPIEL_CHECK_FLOAT_EQ(state->Returns()[0], 1. + std::log(10));
+}
+
+}  // namespace
+}  // namespace crowd_modelling
+}  // namespace open_spiel
+
+int main(int argc, char** argv) {
+  open_spiel::crowd_modelling::TestLoad();
+  open_spiel::crowd_modelling::TestLoadWithParams();
+  open_spiel::crowd_modelling::TestRandomPlay();
+  open_spiel::crowd_modelling::TestReward();
+}
---
+++ open_spiel/python/mfg/games/crowd_modelling_example.py
@@ -0,0 +1,105 @@
+# Copyright 2019 DeepMind Technologies Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Generate a dummy trajectory and compute the distribution of a policy."""
+# pylint: disable=unused-import
+from typing import Sequence
+
+from absl import app
+from absl import flags
+import numpy as np
+
+from open_spiel.python import policy
+from open_spiel.python.mfg.algorithms import best_response_value
+from open_spiel.python.mfg.algorithms import distribution
+from open_spiel.python.mfg.algorithms import fictitious_play
+from open_spiel.python.mfg.algorithms import greedy_policy
+from open_spiel.python.mfg.algorithms import nash_conv
+from open_spiel.python.mfg.algorithms import policy_value
+from open_spiel.python.mfg.games import crowd_modelling
+import pyspiel
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('game', 'mfg_crowd_modelling_2d', 'Game to use.')
+
+
+def main(argv: Sequence[str]) -> None:
+  # TODO(perolat): move to an example directory.
+  if len(argv) > 1:
+    raise app.UsageError('Too many command-line arguments.')
+  game_settings = {
+      'only_distribution_reward': True,
+      'forbidden_states': '[0|0;0|1]',
+      'initial_distribution': '[0|2;0|3]',
+      'initial_distribution_value': '[0.5;0.5]',
+  }
+  mfg_game = pyspiel.load_game(FLAGS.game, game_settings)
+  mfg_state = mfg_game.new_initial_state()
+  while not mfg_state.is_terminal():
+    print(mfg_state.observation_string(0))
+    if mfg_state.current_player() == pyspiel.PlayerId.CHANCE:
+      action_list, prob_list = zip(*mfg_state.chance_outcomes())
+      action = np.random.choice(action_list, p=prob_list)
+      mfg_state.apply_action(action)
+    elif mfg_state.current_player() == pyspiel.PlayerId.MEAN_FIELD:
+      dist_to_register = mfg_state.distribution_support()
+      n_states = len(dist_to_register)
+      dist = [1.0 / n_states for _ in range(n_states)]
+      mfg_state.update_distribution(dist)
+    else:
+      legal_list = mfg_state.legal_actions()
+      action = np.random.choice(legal_list)
+      mfg_state.apply_action(action)
+
+  print('compute nashconv')
+  uniform_policy = policy.UniformRandomPolicy(mfg_game)
+  nash_conv_fp = nash_conv.NashConv(mfg_game, uniform_policy)
+  print(nash_conv_fp.nash_conv())
+
+  print('compute distribution')
+  mfg_dist = distribution.DistributionPolicy(mfg_game, uniform_policy)
+  br_value = best_response_value.BestResponse(mfg_game, mfg_dist)
+  py_value = policy_value.PolicyValue(mfg_game, mfg_dist, uniform_policy)
+  print(br_value(mfg_game.new_initial_state()))
+  print(py_value(mfg_game.new_initial_state()))
+  greedy_pi = greedy_policy.GreedyPolicy(mfg_game, None, br_value)
+  greedy_pi = greedy_pi.to_tabular()
+  pybr_value = policy_value.PolicyValue(mfg_game, mfg_dist, greedy_pi)
+  print(pybr_value(mfg_game.new_initial_state()))
+  print('merge')
+  merged_pi = fictitious_play.MergedPolicy(
+      mfg_game, list(range(mfg_game.num_players())),
+      [uniform_policy, greedy_pi],
+      [mfg_dist, distribution.DistributionPolicy(mfg_game, greedy_pi)],
+      [0.5, 0.5])
+
+  merged_pi_value = policy_value.PolicyValue(mfg_game, mfg_dist, merged_pi)
+  print(br_value(mfg_game.new_initial_state()))
+  print(py_value(mfg_game.new_initial_state()))
+  print(merged_pi_value(mfg_game.new_initial_state()))
+  print((br_value(mfg_game.new_initial_state()) +
+         py_value(mfg_game.new_initial_state())) / 2)
+  print('fp')
+  fp = fictitious_play.FictitiousPlay(mfg_game)
+  for j in range(100):
+    print(j)
+    fp.iteration()
+    fp_policy = fp.get_policy()
+    nash_conv_fp = nash_conv.NashConv(mfg_game, fp_policy)
+    print(nash_conv_fp.nash_conv())
+
+
+if __name__ == '__main__':
+  app.run(main)
