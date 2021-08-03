761a31737d94230c1275d567f80ef1120b729284
Marc Lanctot <marc.lanctot@gmail.com>
2021-08-03 09:27:59 -0230

    Try testing via CIBW_TEST_COMMAND

--- open_spiel/scripts/test_wheel.sh
+++ open_spiel/scripts/test_wheel.sh
@@ -16,31 +16,42 @@
 set -e
 set -x
 
+if [ "$1" = "" ];
+then
+  echo "Usage: test_wheel <project main dir>"
+fi
+
 uname -a
 
-OS=`uname -a | awk '{print $1}'`
-if [[ "$OS" = "Linux" && "$OS_PYTHON_VERSION" = "3.9" ]]; then
-  echo "Linux detected and Python 3.9 requested. Installing Python 3.9 and setting as default."
-  sudo apt-get install python3.9 python3.9-dev
-  sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.9 1
-  sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
-fi
+#OS=`uname -a | awk '{print $1}'`
+#if [[ "$OS" = "Linux" && "$OS_PYTHON_VERSION" = "3.9" ]]; then
+#  echo "Linux detected and Python 3.9 requested. Installing Python 3.9 and setting as default."
+#  sudo apt-get install python3.9 python3.9-dev
+#  sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.9 1
+#  sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
+#fi
 
 PYBIN=${PYBIN:-"python"}
 PYBIN=`which $PYBIN`
+         
+$PYBIN -m pip install --upgrade pip
+$PYBIN -m pip install --upgrade setuptools
+$PYBIN -m pip install --upgrade -r requirements.txt -q
+source $1/open_spiel/scripts/python_extra_deps.sh
+$PYBIN -m pip install --upgrade $OPEN_SPIEL_PYTHON_JAX_DEPS $OPEN_SPIEL_PYTHON_PYTORCH_DEPS $OPEN_SPIEL_PYTHON_TENSORFLOW_DEPS $OPEN_SPIEL_PYTHON_MISC_DEPS
 
-if [[ "$OS" = "Linux" ]]; then
-  ${PYBIN} -m pip install wheelhouse/open_spiel-0.3.1-cp39-cp39-manylinux2014_x86_64.whl
-else
-  ${PYBIN} -m pip install wheelhouse/open_spiel-0.3.1-cp39-cp39-macosx_10_9_x86_64.whl
-fi
+#if [[ "$OS" = "Linux" ]]; then
+#  ${PYBIN} -m pip install wheelhouse/open_spiel-0.3.1-cp39-cp39-manylinux2014_x86_64.whl
+#else
+#  ${PYBIN} -m pip install wheelhouse/open_spiel-0.3.1-cp39-cp39-macosx_10_9_x86_64.whl
+#fi
 
 export OPEN_SPIEL_BUILDING_WHEEL="ON"
 export OPEN_SPIEL_BUILD_WITH_HANABI="ON"
 export OPEN_SPIEL_BUILD_WITH_ACPC="ON"
 
 rm -rf build && mkdir build && cd build
-cmake -DPython3_EXECUTABLE=${PYBIN} ../open_spiel
+cmake -DPython3_EXECUTABLE=${PYBIN} $1/open_spiel
 
 NPROC="nproc"
 if [[ "$OS" == "darwin"* ]]; then
