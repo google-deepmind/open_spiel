3272b79023f29932e72beb941d7e6e51688ffd60
lanctot <lanctot@google.com>
2021-10-17 10:05:47 -0230

    Try using brew switch to get python version

--- open_spiel/scripts/install.sh
+++ open_spiel/scripts/install.sh
@@ -258,9 +258,10 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then  # Mac OSX
   # On Github Actions, macOS 10.15 comes with Python 3.10 (which we don't support yet).
   # We want to test multiple Python versions determined by OS_PYTHON_VERSION.
   if [[ "$CI" ]]; then
-    brew install "python@${OS_PYTHON_VERSION}"
-    brew unlink python@3.10
-    brew link --force --overwrite "python@${OS_PYTHON_VERSION}"
+    # brew install "python@${OS_PYTHON_VERSION}"
+    # brew unlink python@3.10
+    # brew link --force --overwrite "python@${OS_PYTHON_VERSION}"
+    brew switch python ${OS_PYTHON_VERSION}
   fi
   `python3 -c "import tkinter" > /dev/null 2>&1` || brew install tcl-tk || echo "** Warning: failed 'brew install tcl-tk' -- continuing"
   python3 --version
