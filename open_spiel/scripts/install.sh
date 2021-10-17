ecb06f1ee50e0bfc0d1e53ca223d3c22e875d95a
lanctot <lanctot@google.com>
2021-10-17 12:10:05 -0230

    Change install pip commands on MacOS

--- open_spiel/scripts/install.sh
+++ open_spiel/scripts/install.sh
@@ -270,9 +270,9 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then  # Mac OSX
     [[ -x `which go` ]] || brew install golang || echo "** Warning: failed 'brew install golang' -- continuing"
   fi
 
-  curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-  python3 get-pip.py
-  pip3 install virtualenv
+  python3 -m ensurepip
+  python3 -m pip install --upgrade pip
+  python3 -m pip install virtualenv
 else
   echo "The OS '$OSTYPE' is not supported (Only Linux and MacOS is). " \
        "Feel free to contribute the install for a new OS."
