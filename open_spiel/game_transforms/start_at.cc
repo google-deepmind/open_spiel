43e9f448695030833471702772c7dba325584dec
Edward Lockhart <locked@google.com>
2021-12-23 11:22:43 -0700

    Update copyright headers to be per-file, instead of generated on export.
    PiperOrigin-RevId: 418029666
    Change-Id: I5c00c7324b5a6e708189becfd28196b383f71a43

--- open_spiel/game_transforms/start_at.cc
+++ open_spiel/game_transforms/start_at.cc
@@ -12,6 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Copyright 2021 DeepMind Technologies Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "open_spiel/game_transforms/start_at.h"
 
 #include "open_spiel/game_transforms/game_wrapper.h"
@@ -63,7 +77,7 @@ std::vector<Action> HistoryFromString(const std::string& str) {
   std::vector<Action> history;
   if (str.empty()) return history;  // Identity transformation.
 
-  std::vector<std::string_view> str_actions =
+  std::vector<absl::string_view> str_actions =
       absl::StrSplit(str, kActionSeparator);
   for (const auto& str_action : str_actions) {
     Action a;
