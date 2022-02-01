60b5a344ce4b31478252ce1a1ecf530ff87d37d4
Max <m.spahn@tudelft.nl>
2022-02-01 15:33:14 +0100

    Adds parameters to the examples for quoridor testing. Temporary change.

--- open_spiel/examples/example.cc
+++ open_spiel/examples/example.cc
@@ -26,6 +26,8 @@ ABSL_FLAG(int, players, 3, "How many players in this game, 0 for default.");
 ABSL_FLAG(bool, show_infostate, false, "Show the information state.");
 ABSL_FLAG(int, seed, 0, "Seed for the random number generator. 0 for auto.");
 ABSL_FLAG(bool, show_legals, false, "Show the legal moves.");
+ABSL_FLAG(int, wall_count, 10, "Wall count for quoridor. 0 for auto.");
+ABSL_FLAG(int, board_size, 9, "Board size for quoridor. 0 for auto.");
 
 void PrintLegalActions(const open_spiel::State& state,
                        open_spiel::Player player,
@@ -44,6 +46,8 @@ int main(int argc, char** argv) {
   bool show_infostate = absl::GetFlag(FLAGS_show_infostate);
   int seed = absl::GetFlag(FLAGS_seed);
   bool show_legals = absl::GetFlag(FLAGS_show_legals);
+  auto wall_count = absl::GetFlag(FLAGS_wall_count);
+  auto board_size = absl::GetFlag(FLAGS_board_size);
 
   // Print out registered games.
   std::cerr << "Registered games:" << std::endl;
@@ -60,12 +64,9 @@ int main(int argc, char** argv) {
 
   // Add any specified parameters to override the defaults.
   open_spiel::GameParameters params;
-  if (players > 0) {
-    params["num_players"] = open_spiel::GameParameter(players);
-  }
-  params["num_players"] = open_spiel::GameParameter(4);
-  params["wall_count"] = open_spiel::GameParameter(1);
-  params["board_size"] = open_spiel::GameParameter(5);
+  params["num_players"] = open_spiel::GameParameter(players);
+  params["wall_count"] = open_spiel::GameParameter(wall_count);
+  params["board_size"] = open_spiel::GameParameter(board_size);
   std::shared_ptr<const open_spiel::Game> game =
       open_spiel::LoadGame(game_name, params);
 
