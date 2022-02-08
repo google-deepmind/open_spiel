58a98abac82f8f98997d6cca814ed4ad7bc95347
Sertan Girgin <sertan@google.com>
2022-02-08 04:03:24 -0700

    The reservoir buffer class that was used in Neural Fictitious Self Play (NFSP) has been put in a separate file under python/utils.
    PiperOrigin-RevId: 427144390
    Change-Id: Id119f75afbffdfa4533993d09081e56c2a387896

--- open_spiel/python/jax/nfsp.py
+++ open_spiel/python/jax/nfsp.py
@@ -28,7 +28,6 @@ import collections
 import contextlib
 import enum
 import os
-import random
 
 import haiku as hk
 import jax
@@ -38,7 +37,7 @@ import optax
 
 from open_spiel.python import rl_agent
 from open_spiel.python.jax import dqn
-
+from open_spiel.python.utils.reservoir_buffer import ReservoirBuffer
 
 Transition = collections.namedtuple(
     "Transition", "info_state action_probs legal_actions_mask")
@@ -327,60 +326,3 @@ class NFSP(rl_agent.AbstractAgent):
       checkpoint_dir: directory from which checkpoints will be restored.
     """
     raise NotImplementedError
-
-
-# TODO(perolat): refactor the reservoir with the NFSP Pytorch implementation
-class ReservoirBuffer(object):
-  """Allows uniform sampling over a stream of data.
-
-  This class supports the storage of arbitrary elements, such as observation
-  tensors, integer actions, etc.
-
-  See https://en.wikipedia.org/wiki/Reservoir_sampling for more details.
-  """
-
-  def __init__(self, reservoir_buffer_capacity):
-    self._reservoir_buffer_capacity = reservoir_buffer_capacity
-    self._data = []
-    self._add_calls = 0
-
-  def add(self, element):
-    """Potentially adds `element` to the reservoir buffer.
-
-    Args:
-      element: data to be added to the reservoir buffer.
-    """
-    if len(self._data) < self._reservoir_buffer_capacity:
-      self._data.append(element)
-    else:
-      idx = np.random.randint(0, self._add_calls + 1)
-      if idx < self._reservoir_buffer_capacity:
-        self._data[idx] = element
-    self._add_calls += 1
-
-  def sample(self, num_samples):
-    """Returns `num_samples` uniformly sampled from the buffer.
-
-    Args:
-      num_samples: `int`, number of samples to draw.
-
-    Returns:
-      An iterable over `num_samples` random elements of the buffer.
-
-    Raises:
-      ValueError: If there are less than `num_samples` elements in the buffer
-    """
-    if len(self._data) < num_samples:
-      raise ValueError("{} elements could not be sampled from size {}".format(
-          num_samples, len(self._data)))
-    return random.sample(self._data, num_samples)
-
-  def clear(self):
-    self._data = []
-    self._add_calls = 0
-
-  def __len__(self):
-    return len(self._data)
-
-  def __iter__(self):
-    return iter(self._data)
---
+++ open_spiel/python/utils/reservoir_buffer.py
@@ -0,0 +1,68 @@
+"""Reservoir buffer implemented in Numpy.
+
+See https://en.wikipedia.org/wiki/Reservoir_sampling for more details.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import random
+import numpy as np
+
+
+# TODO(perolat): refactor the reservoir with the NFSP Pytorch implementation
+class ReservoirBuffer(object):
+  """Allows uniform sampling over a stream of data.
+
+  This class supports the storage of arbitrary elements, such as observation
+  tensors, integer actions, etc.
+
+  See https://en.wikipedia.org/wiki/Reservoir_sampling for more details.
+  """
+
+  def __init__(self, reservoir_buffer_capacity):
+    self._reservoir_buffer_capacity = reservoir_buffer_capacity
+    self._data = []
+    self._add_calls = 0
+
+  def add(self, element):
+    """Potentially adds `element` to the reservoir buffer.
+
+    Args:
+      element: data to be added to the reservoir buffer.
+    """
+    if len(self._data) < self._reservoir_buffer_capacity:
+      self._data.append(element)
+    else:
+      idx = np.random.randint(0, self._add_calls + 1)
+      if idx < self._reservoir_buffer_capacity:
+        self._data[idx] = element
+    self._add_calls += 1
+
+  def sample(self, num_samples):
+    """Returns `num_samples` uniformly sampled from the buffer.
+
+    Args:
+      num_samples: `int`, number of samples to draw.
+
+    Returns:
+      An iterable over `num_samples` random elements of the buffer.
+
+    Raises:
+      ValueError: If there are less than `num_samples` elements in the buffer
+    """
+    if len(self._data) < num_samples:
+      raise ValueError("{} elements could not be sampled from size {}".format(
+          num_samples, len(self._data)))
+    return random.sample(self._data, num_samples)
+
+  def clear(self):
+    self._data = []
+    self._add_calls = 0
+
+  def __len__(self):
+    return len(self._data)
+
+  def __iter__(self):
+    return iter(self._data)
