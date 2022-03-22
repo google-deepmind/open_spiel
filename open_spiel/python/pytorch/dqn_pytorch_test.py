7ae4f9c9473b2fbad571cd8343944328720e8d89
Marc Lanctot <marc.lanctot@gmail.com>
2022-03-21 21:42:16 -0230

    Change values in pytorch DQN test

--- open_spiel/python/pytorch/dqn_pytorch_test.py
+++ open_spiel/python/pytorch/dqn_pytorch_test.py
@@ -46,18 +46,18 @@ class DQNTest(absltest.TestCase):
                     hidden_layers_sizes=[16],
                     replay_buffer_capacity=100,
                     batch_size=5,
-                    epsilon_start=0.02,
+                    epsilon_start=0.1,
                     epsilon_end=0.01)
     total_reward = 0
 
-    for _ in range(100):
+    for _ in range(1000):
       time_step = env.reset()
       while not time_step.last():
         agent_output = agent.step(time_step)
         time_step = env.step([agent_output.action])
         total_reward += time_step.rewards[0]
       agent.step(time_step)
-    self.assertGreaterEqual(total_reward, 75)
+    self.assertGreaterEqual(total_reward, 250)
 
   def test_run_tic_tac_toe(self):
     env = rl_environment.Environment("tic_tac_toe")
