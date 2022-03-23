0e5d33c50bf08cae192e6b9d9fec34cec8d66812
Joppe <joppdj@gmail.com>
2022-03-23 20:28:01 +0100

    move boltzmann Q learner to subclass

--- open_spiel/python/algorithms/tabular_qlearner.py
+++ open_spiel/python/algorithms/tabular_qlearner.py
@@ -53,8 +53,9 @@ class QLearner(rl_agent.AbstractAgent):
         self._q_values = collections.defaultdict(valuedict)
         self._prev_info_state = None
         self._last_loss_value = None
+        self._current_epsilon = None
 
-    def _epsilon_greedy(self, info_state, legal_actions, epsilon):
+    def _epsilon_greedy(self, info_state, legal_actions):
         """Returns a valid epsilon-greedy action and valid action probs.
 
     If the agent has not been to `info_state`, a valid random action is chosen.
@@ -72,10 +73,17 @@ class QLearner(rl_agent.AbstractAgent):
         greedy_actions = [
             a for a in legal_actions if self._q_values[info_state][a] == greedy_q
         ]
-        probs[legal_actions] = epsilon / len(legal_actions)
-        probs[greedy_actions] += (1 - epsilon) / len(greedy_actions)
+        probs[legal_actions] = self._current_epsilon / len(legal_actions)
+        probs[greedy_actions] += (1 - self._current_epsilon) / len(greedy_actions)
         action = np.random.choice(range(self._num_actions), p=probs)
         return action, probs
+
+    def _get_action_probs(self, info_state, legal_actions):
+        """Returns a selected action and the probabilities of legal actions.
+
+        To be overwritten by subclasses that implement other action selection methods.
+        """
+        return self._epsilon_greedy(info_state, legal_actions)
 
     def step(self, time_step, is_evaluation=False):
         """Returns the action to be taken and updates the Q-values if needed.
@@ -98,9 +106,9 @@ class QLearner(rl_agent.AbstractAgent):
 
         # Act step: don't act at terminal states.
         if not time_step.last():
-            epsilon = 0.0 if is_evaluation else self._epsilon
-            action, probs = self._epsilon_greedy(
-                info_state, legal_actions, epsilon=epsilon)
+            self._current_epsilon = 0.0 if is_evaluation else self._epsilon
+            action, probs = self._get_action_probs(
+                info_state, legal_actions)
 
         # Learn step: don't learn during evaluation or at first agent steps.
         if self._prev_info_state and not is_evaluation:
@@ -116,125 +124,6 @@ class QLearner(rl_agent.AbstractAgent):
 
             # Decay epsilon, if necessary.
             self._epsilon = self._epsilon_schedule.step()
-
-            if time_step.last():  # prepare for the next episode.
-                self._prev_info_state = None
-                return
-
-        # Don't mess up with the state during evaluation.
-        if not is_evaluation:
-            self._prev_info_state = info_state
-            self._prev_action = action
-        return rl_agent.StepOutput(action=action, probs=probs)
-
-    @property
-    def loss(self):
-        return self._last_loss_value
-
-
-class BoltzmannQLearner(rl_agent.AbstractAgent):
-    """Tabular Boltzmann Q-Learning agent.
-
-    See open_spiel/python/examples/tic_tac_toe_qlearner.py for an usage example.
-
-    The tic_tac_toe example uses the standard Qlearner. Using the BoltzmannQlearner is
-    identical and only differs in the initialization of the agents.
-    """
-
-    def __init__(self,
-                 player_id,
-                 num_actions,
-                 step_size=0.1,
-                 discount_factor=1.0,
-                 temperature_schedule=rl_tools.ConstantSchedule(1.),
-                 centralized=False):
-        """Initialize the Q-Learning agent."""
-        self._player_id = player_id
-        self._num_actions = num_actions
-        self._step_size = step_size
-        self._discount_factor = discount_factor
-        self._temperature_schedule = temperature_schedule
-        self._temperature = temperature_schedule.value
-        self._centralized = centralized
-        self._q_values = collections.defaultdict(valuedict)
-        self._prev_info_state = None
-        self._last_loss_value = None
-
-    def _boltzmann_selection(self, info_state, legal_actions, temperature):
-        """Action selection based on boltzmann probability interpretation of Q-values.
-
-        For more details, see equation (2) page 2 in
-        https://arxiv.org/pdf/1109.1528.pdf
-
-        Args:
-            info_state: hashable representation of the information state.
-            legal_actions: list of actions at `info_state`.
-            temperature: float, the higher the value, the higher the chance of taking an exploratory action.
-
-        Returns:
-            A valid Boltzmann selected action and valid action probabilities.
-        """
-        probs = np.zeros(self._num_actions)
-
-        if temperature != 0.0:
-            probs += [
-                np.exp((1 / temperature) * self._q_values[info_state][i])
-                for i in range(self._num_actions)
-            ]
-            probs /= np.sum(
-                [np.exp((1 / temperature) * self._q_values[info_state][i]) for i in range(self._num_actions)]
-            )
-        else:
-            # Temperature = 0 causes normal greedy action selection
-            greedy_q = max([self._q_values[info_state][a] for a in legal_actions])
-            greedy_actions = [
-                a for a in legal_actions if self._q_values[info_state][a] == greedy_q
-            ]
-
-            probs[greedy_actions] += 1 / len(greedy_actions)
-
-        action = np.random.choice(range(self._num_actions), p=probs)
-        return action, probs
-
-    def step(self, time_step, is_evaluation=False):
-        """Returns the action to be taken and updates the Q-values if needed.
-
-  Args:
-    time_step: an instance of rl_environment.TimeStep.
-    is_evaluation: bool, whether this is a training or evaluation call.
-
-  Returns:
-    A `rl_agent.StepOutput` containing the action probs and chosen action.
-  """
-        if self._centralized:
-            info_state = str(time_step.observations["info_state"])
-        else:
-            info_state = str(time_step.observations["info_state"][self._player_id])
-        legal_actions = time_step.observations["legal_actions"][self._player_id]
-
-        # Prevent undefined errors if this agent never plays until terminal step
-        action, probs = None, None
-
-        # Act step: don't act at terminal states.
-        if not time_step.last():
-            temperature = 0.0 if is_evaluation else self._temperature
-            action, probs = self._boltzmann_selection(
-                info_state, legal_actions, temperature=temperature)
-
-        # Learn step: don't learn during evaluation or at first agent steps.
-        if self._prev_info_state and not is_evaluation:
-            target = time_step.rewards[self._player_id]
-            if not time_step.last():  # Q values are zero for terminal.
-                target += self._discount_factor * max(
-                    [self._q_values[info_state][a] for a in legal_actions])
-
-            prev_q_value = self._q_values[self._prev_info_state][self._prev_action]
-            self._last_loss_value = target - prev_q_value
-            self._q_values[self._prev_info_state][self._prev_action] += (
-                    self._step_size * self._last_loss_value)
-
-            # Decay temperature, if necessary.
-            self._temperature = self._temperature_schedule.step()
 
             if time_step.last():  # prepare for the next episode.
                 self._prev_info_state = None
