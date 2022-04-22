c8391ea65be858357feb2921aa0217a8119eb9ad
Marc Lanctot <marc.lanctot@gmail.com>
2022-04-21 20:32:45 -0600

    Upgrade versions of tensorflow-probability, tensorflow-datasets

--- open_spiel/scripts/python_extra_deps.sh
+++ open_spiel/scripts/python_extra_deps.sh
@@ -26,5 +26,5 @@
 # scripts/global_variables.sh
 export OPEN_SPIEL_PYTHON_JAX_DEPS="jax==0.3.7 dm-haiku==0.0.6 optax==0.1.2 chex==0.1.3 rlax==0.1.2"
 export OPEN_SPIEL_PYTHON_PYTORCH_DEPS="torch==1.11.0"
-export OPEN_SPIEL_PYTHON_TENSORFLOW_DEPS="numpy==1.21.5 tensorflow==2.8.0 tensorflow-probability<0.8.0,>=0.7.0 tensorflow_datasets==4.3.0 keras==2.8.0"
+export OPEN_SPIEL_PYTHON_TENSORFLOW_DEPS="numpy==1.21.5 tensorflow==2.8.0 tensorflow-probability==0.16.0 tensorflow_datasets==4.5.2 keras==2.8.0"
 export OPEN_SPIEL_PYTHON_MISC_DEPS="IPython==5.8.0 cvxopt==1.2.7 networkx==2.4 matplotlib==3.3.2 mock==4.0.2 nashpy==0.0.19 scipy==1.7.3 testresources==2.0.1 cvxpy==1.1.17 ecos==2.0.7.post1 osqp==0.6.2.post0 clu==0.0.6"
