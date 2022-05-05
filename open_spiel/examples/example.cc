ddb17bcc2024a7dddc240bb2328517ffda1d087e
Theophile Cabannes <cabannes@google.com>
2022-05-05 07:24:34 -0600

    Add mean field node in example.
    PiperOrigin-RevId: 446700589
    Change-Id: I096fe5bc7877a5450d0c8e8c7f5ad29dc60098a8

--- open_spiel/examples/example.cc
+++ open_spiel/examples/example.cc
@@ -89,6 +89,12 @@ int main(int argc, char** argv) {
                 << state->ActionToString(open_spiel::kChancePlayerId, action)
                 << std::endl;
       state->ApplyAction(action);
+    } else if (state->IsMeanFieldNode()) {
+      int num_states_distribution = state->DistributionSupport().size();
+      if (num_states_distribution) {
+        state->UpdateDistribution(std::vector<double>(
+            num_states_distribution, 1.0 / num_states_distribution));
+      }
     } else if (state->IsSimultaneousNode()) {
       // open_spiel::Players choose simultaneously?
       std::vector<open_spiel::Action> joint_action;
@@ -115,7 +121,7 @@ int main(int argc, char** argv) {
         }
 
         open_spiel::Action action = 0;
-        if (!actions.empty()){
+        if (!actions.empty()) {
           absl::uniform_int_distribution<> dis(0, actions.size() - 1);
           action = actions[dis(rng)];
         }
