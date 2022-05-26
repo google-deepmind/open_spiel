5aa2b1492d6bdf091432c2553614986aea18c57d
Saber Haj Rabiee <sabify@users.noreply.github.com>
2022-05-26 10:11:55 -0700

    Update dqn.cc

--- open_spiel/algorithms/dqn_torch/dqn.cc
+++ open_spiel/algorithms/dqn_torch/dqn.cc
@@ -31,7 +31,8 @@ namespace open_spiel {
 namespace algorithms {
 namespace torch_dqn {
 
-constexpr const int kIllegalActionLogitsPenalty = -1e9;
+constexpr const float kIllegalActionLogitsPenalty =
+    std::numeric_limits<float>::lowest();
 
 Action RandomAgent::Step(const State& state, bool is_evaluation) {
   if (state.IsTerminal()) {
@@ -216,7 +217,6 @@ void DQN::Learn() {
         torch::from_blob(t.legal_actions_mask.data(),
                          {1, t.legal_actions_mask.size()},
                          torch::TensorOptions().dtype(torch::kInt32))
-            .to(torch::kInt64)
             .clone());
     actions.push_back(t.action);
     rewards.push_back(t.reward);
@@ -231,11 +231,12 @@ void DQN::Learn() {
       next_info_states_tensor).detach();
 
   torch::Tensor legal_action_masks_tensor = torch::stack(legal_actions_mask, 0);
-  torch::Tensor illegal_actions = 1.0 - legal_action_masks_tensor;
-  torch::Tensor illegal_logits = illegal_actions * kIllegalActionLogitsPenalty;
+  legal_action_masks_tensor = 1.0 - legal_action_masks_tensor;
+  torch::Tensor legal_q_values = torch::masked_fill(
+      target_q_values, legal_action_masks_tensor, kIllegalActionLogitsPenalty);
 
-  torch::Tensor max_next_q = std::get<0>(
-      torch::max(target_q_values + illegal_logits, 2));
+  torch::Tensor max_next_q = std::get<0>(legal_q_values.max(2));
+
   torch::Tensor are_final_steps_tensor = torch::from_blob(
       are_final_steps.data(),
       {batch_size_},
