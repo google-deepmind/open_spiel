11018e29b75cc84de1b987419be41f52778056fa
lizun <rezunli96@gmail.com>
2022-06-18 00:21:01 -0400

    fix bug in nash avg and add test

--- open_spiel/python/algorithms/nash_averaging_test.py
+++ open_spiel/python/algorithms/nash_averaging_test.py
@@ -32,14 +32,26 @@ value_trans = np.asarray([-1., -1., 0.])
 game_rps = pyspiel.create_matrix_game(
     [[0.0, -1.0, 1.0], [1.0, 0.0, -1.0], [-1.0, 1.0, 0.0]],
     [[0.0, 1.0, -1.0], [-1.0, 0.0, 1.0], [1.0, -1.0, 0.0]])
-eq_rps = np.asarray([1/3, 1/3, 1/3])
+eq_rps = np.asarray([1 / 3, 1 / 3, 1 / 3])
 value_rps = np.asarray([0., 0., 0.])
 
 
+# game with one dominated strategy
+p_mat0 = np.asarray([
+    [0.0,  234.,   34., -270.],
+    [-234.,    0.,  -38., -464.],
+    [-34.,   38.,    0., -270.],
+    [270.,  464.,  270.,    0.]
+])
+game0 = pyspiel.create_matrix_game(p_mat0, -p_mat0)
+dominated_idxs0 = [0, 1, 2]
+
+
 class NashAveragingTest(parameterized.TestCase):
+
   @parameterized.named_parameters(
-      ('transitive_game', game_trans, eq_trans, value_trans),
-      ('rps_game', game_rps, eq_rps, value_rps),
+      ("transitive_game", game_trans, eq_trans, value_trans),
+      ("rps_game", game_rps, eq_rps, value_rps),
   )
   def test_simple_games(self, game, eq, value):
 
@@ -49,6 +61,15 @@ class NashAveragingTest(parameterized.TestCase):
 
     with self.subTest("value"):
       np.testing.assert_array_almost_equal(value, nash_avg_value.reshape(-1))
+
+  @parameterized.named_parameters(
+      ("game0", game0, dominated_idxs0),
+  )
+  def test_games_with_dominated_strategy(self, game, dominated_idxs0):
+    maxent_nash, _ = nash_averaging(game)
+    with self.subTest("dominated strategies have zero Nash probs"):
+      for idx in dominated_idxs0:
+        self.assertAlmostEqual(maxent_nash[idx].item(), 0.0)
 
 
 if __name__ == "__main__":
