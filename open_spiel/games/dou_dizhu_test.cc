7a9541b8b1be66d9f4bca89e9d3ec61be5d03511
lizun <rezunli96@gmail.com>
2022-12-06 17:48:31 -0500

    remove commented #include

--- open_spiel/games/dou_dizhu_test.cc
+++ open_spiel/games/dou_dizhu_test.cc
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// #include "open_spiel/abseil-cpp/absl/types/optional.h"
-// #include "open_spiel/spiel.h"
 #include "open_spiel/spiel.h"
 #include "open_spiel/tests/basic_tests.h"
 
