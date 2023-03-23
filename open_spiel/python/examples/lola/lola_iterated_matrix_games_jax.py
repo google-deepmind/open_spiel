3e800e1192d32f178d15bd17134853be71a20f5b
Axel Brunnbauer <axel.brunnbauer@gmx.at>
2023-03-23 18:31:49 +0100

    finished experiments

--- open_spiel/python/examples/lola/lola_iterated_matrix_games_jax.py
+++ open_spiel/python/examples/lola/lola_iterated_matrix_games_jax.py
@@ -1,4 +1,5 @@
 import itertools
+import os
 import typing
 import warnings
 from typing import List, Tuple
@@ -11,11 +12,11 @@ import jax.tree_util
 import numpy as np
 from absl import app
 from absl import flags
-from aim import Run
+import wandb
+
 
 from open_spiel.python.environments.iterated_matrix_game import IteratedPrisonersDilemma, IteratedMatchingPennies
 from open_spiel.python.jax.lola_jax import LolaPolicyGradientAgent
-from open_spiel.python.jax.policy_gradient import PolicyGradient
 from open_spiel.python.rl_environment import Environment, TimeStep
 
 warnings.simplefilter('ignore', FutureWarning)
@@ -25,28 +26,30 @@ Example that trains two agents using LOLA (Foerster et al., 2017) and LOLA-DiCE 
 on iterated matrix games. Hyperparameters are taken from the paper and https://github.com/alexis-jacq/LOLA_DiCE.
 """
 FLAGS = flags.FLAGS
+flags.DEFINE_string("exp_name", 'dice_1step_pytorchparams', "Experiment name.")
 flags.DEFINE_integer("seed", 42, "Random seed.")
 flags.DEFINE_string("game", "ipd", "Name of the game.")
 flags.DEFINE_integer("epochs", 200, "Number of training iterations.")
-flags.DEFINE_integer("batch_size", 4096, "Number of episodes in a batch.")
+flags.DEFINE_integer("batch_size", 128, "Number of episodes in a batch.")
 flags.DEFINE_integer("critic_mini_batches", 1, "Number of minibatches for critic.")
 flags.DEFINE_integer("game_iterations", 150, "Number of iterated plays.")
-flags.DEFINE_float("policy_lr", 0.3, "Policy learning rate.")
+flags.DEFINE_float("policy_lr", 0.2, "Policy learning rate.")
 flags.DEFINE_float("opp_policy_lr", 0.3, "Policy learning rate.")
-flags.DEFINE_float("critic_lr", 0.9, "Critic learning rate.")
-flags.DEFINE_string("correction_type", 'none', "Either 'lola', 'dice' or 'none'.")
-flags.DEFINE_integer("n_lookaheads", 1, "Number of lookaheads for LOLA correction.")
+flags.DEFINE_float("critic_lr", 0.1, "Critic learning rate.")
+flags.DEFINE_string("correction_type", 'dice', "Either 'lola', 'dice' or None.")
+flags.DEFINE_integer("n_lookaheads", 2, "Number of lookaheads for LOLA correction.")
 flags.DEFINE_float("correction_max_grad_norm", None, "Maximum gradient norm of LOLA correction.")
 flags.DEFINE_float("discount", 0.96, "Discount factor.")
 flags.DEFINE_integer("policy_update_interval", 1, "Number of critic updates per before policy is updated.")
-flags.DEFINE_integer("eval_batch_size", 30, "Random seed.")
-flags.DEFINE_bool("use_jit", True, "If true, JAX jit compilation will be enabled.")
-flags.DEFINE_bool("use_opponent_modelling", False, "If false, ground truth opponent weights are used.")
+flags.DEFINE_integer("eval_batch_size", 1024, "Random seed.")
+flags.DEFINE_bool("use_jit", False, "If true, JAX jit compilation will be enabled.")
+flags.DEFINE_bool("use_opponent_modelling", True, "If false, ground truth opponent weights are used.")
 flags.DEFINE_integer("opp_policy_mini_batches", 8, "Number of minibatches for opponent policy.")
+flags.DEFINE_float("opponent_model_learning_rate", 0.3, "Learning rate for opponent model.")
 
 def get_action_probs(agent: LolaPolicyGradientAgent, game: str) -> List[typing.Dict[str, typing.Any]]:
     actions = ['C', 'D'] if game == 'ipd' else ['H', 'T']
-    states = [''.join(s) for s in itertools.product(actions, repeat=2)] + ['s0']
+    states = ['s0'] + [''.join(s) for s in itertools.product(actions, repeat=2)]
     params = agent.train_state.policy_params[agent.player_id]
     action_probs = []
     for i, s in enumerate(states):
@@ -55,19 +58,21 @@ def get_action_probs(agent: LolaPolicyGradientAgent, game: str) -> List[typing.D
         action = actions[0]
         action_probs.append(dict(prob=prob.item(), name=f'P({action}|{s})'))
     return action_probs
-def log_epoch_data(run: Run, epoch: int, agents: List[LolaPolicyGradientAgent], eval_batch):
-
+def log_epoch_data(epoch: int, agents: List[LolaPolicyGradientAgent], eval_batch):
+    logs = {}
     for agent in agents:
         avg_step_reward = np.mean([ts.rewards[agent.player_id] for ts in eval_batch])
         probs = get_action_probs(agent, game=FLAGS.game)
         for info in probs:
-            run.track(info['prob'], name=info['name'], context={'agent': agent.player_id})
+            logs[f'agent_{agent.player_id}/{info["name"]}'] = info['prob']
         probs = ', '.join([f'{info["name"]}: {info["prob"]:.2f}' for info in probs])
         metrics = agent.metrics()
-        for k, v in metrics.items():
-            run.track(v.item(), name=k, context={'agent': agent.player_id})
-        run.track(avg_step_reward, name='avg_step_reward', context={'agent': agent.player_id})
+        logs.update({
+            f'agent_{agent.player_id}/avg_step_reward': avg_step_reward,
+            **{f'agent_{agent.player_id}/{k}': v.item() for k, v in metrics.items()}
+        })
         print(f'[epoch {epoch}] Agent {agent.player_id}: {avg_step_reward:.2f} | {probs}')
+    wandb.log(logs)
 
 
 def collect_batch(env: Environment, agents: List[LolaPolicyGradientAgent], eval: bool):
@@ -108,6 +113,7 @@ def make_agent(key: jax.random.PRNGKey, player_id: int, env: Environment,
         opp_policy_learning_rate=FLAGS.opp_policy_lr,
         num_opponent_updates=FLAGS.opp_policy_mini_batches,
         critic_learning_rate=FLAGS.critic_lr,
+        opponent_model_learning_rate=FLAGS.opponent_model_learning_rate,
         policy_update_interval=FLAGS.policy_update_interval,
         discount=FLAGS.discount,
         critic_discount=0, # Predict only the immediate reward (only for iterated matrix games)
@@ -154,23 +160,32 @@ def update_weights(agents: List[LolaPolicyGradientAgent]):
 
 
 def main(_):
-    run = Run(experiment=f'opponent_shaping_{FLAGS.game}_{FLAGS.correction_type}')
-    run["hparams"] = {
-        "seed": FLAGS.seed,
-        "batch_size": FLAGS.batch_size,
-        "game_iterations": FLAGS.game_iterations,
-        "with_opp_modelling": FLAGS.use_opponent_modelling,
-        "discount": FLAGS.discount,
-        "policy_lr": FLAGS.policy_lr,
-        "opp_policy_lr": FLAGS.opp_policy_lr,
-        "critic_lr": FLAGS.critic_lr,
-        "policy_update_interval": FLAGS.policy_update_interval,
-        "correction_type": FLAGS.correction_type,
-        "correction_max_grad_norm": FLAGS.correction_max_grad_norm,
-        "n_lookaheads": FLAGS.n_lookaheads,
-        "use_jit": FLAGS.use_jit
-    }
-
+    if FLAGS.exp_name is None:
+        FLAGS.exp_name = f'{FLAGS.game}_{FLAGS.seed}'
+    wandb.login(key=os.environ.get('WANDB_API_KEY', None))
+    wandb.init(
+        project='open-spiel-opponent-modelling',
+        group=FLAGS.exp_name,
+        config={
+            'game': FLAGS.game,
+            'seed': FLAGS.seed,
+            'epochs': FLAGS.epochs,
+            'batch_size': FLAGS.batch_size,
+            'critic_mini_batches': FLAGS.critic_mini_batches,
+            'game_iterations': FLAGS.game_iterations,
+            'policy_lr': FLAGS.policy_lr,
+            'opp_policy_lr': FLAGS.opp_policy_lr,
+            'critic_lr': FLAGS.critic_lr,
+            'correction_type': FLAGS.correction_type,
+            'n_lookaheads': FLAGS.n_lookaheads,
+            'correction_max_grad_norm': FLAGS.correction_max_grad_norm,
+            'discount': FLAGS.discount,
+            'policy_update_interval': FLAGS.policy_update_interval,
+            'use_opponent_modelling': FLAGS.use_opponent_modelling,
+            'opp_policy_mini_batches': FLAGS.opp_policy_mini_batches,
+            'opponent_model_learning_rate': FLAGS.opponent_model_learning_rate
+        }
+    )
 
     rng = hk.PRNGSequence(key_or_seed=FLAGS.seed)
     env = make_env(iterations=FLAGS.game_iterations, batch_size=FLAGS.batch_size, game=FLAGS.game)
@@ -180,13 +195,15 @@ def main(_):
         update_weights(agents)
 
     batch = collect_batch(env=env, agents=agents, eval=True)
-    log_epoch_data(epoch=0, agents=agents, run=run, eval_batch=batch)
+    log_epoch_data(epoch=0, agents=agents, eval_batch=batch)
     for epoch in range(1, FLAGS.epochs+1):
         batch = collect_batch(env=env, agents=agents, eval=False)
         if not FLAGS.use_opponent_modelling:
             update_weights(agents)
-        log_epoch_data(epoch=epoch, agents=agents, run=run, eval_batch=batch)
+        log_epoch_data(epoch=epoch, agents=agents, eval_batch=batch)
         print('#' * 100)
+
+    wandb.finish()
 
 if __name__ == "__main__":
     app.run(main)
