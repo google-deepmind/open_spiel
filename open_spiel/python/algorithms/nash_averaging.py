fix bug in computing nash averaging; add more tests

open_spiel/python/algorithms/nash_averaging.py
@@ -23,7 +23,7 @@ import numpy as np
 from open_spiel.python.egt.utils import game_payoffs_array
 
 
-def _max_entropy_symmetric_nash(p_mat, num_tasks=None, eps=1e-9):
+def _max_entropy_symmetric_nash(p_mat, eps=1e-9):
   """Solves for the maxent symmetric nash for symmetric 2P zero-sum games.
 
     Using convex programming:
@@ -31,16 +31,10 @@ def _max_entropy_symmetric_nash(p_mat, num_tasks=None, eps=1e-9):
       s.t.
       p_mat.dot(p) <= 0, since game value must be 0
       p >= 0
-      (in Agent-vs-Agent setting)
       1^T * p = 1
-      (in Agent-vs-Task setting)
-      1^T * p[:-num_tasks] = 1
-      1^T * p[-num_tasks:] = 1
 
   Args:
     p_mat: an N*N anti-symmetric payoff matrix for the row player
-    num_tasks: if None or 0, then it is Agent-vs-Agent case
-    otherwise, there are num_tasks in Agent-vs-Task case
     eps: minimum probability threshold
 
   Returns:
@@ -51,17 +45,58 @@ def _max_entropy_symmetric_nash(p_mat, num_tasks=None, eps=1e-9):
   x = cp.Variable(shape=n)
   obj = cp.Maximize(cp.sum(cp.entr(x)))
   constraints = [p_mat @ x <= 0, x >= eps * np.ones(n)]
-  if num_tasks:
-    constraints.append(cp.sum(x[:-num_tasks]) == 1)
-    constraints.append(cp.sum(x[-num_tasks:]) == 1)
-  else:
-    constraints.append(cp.sum(x) == 1)
+  constraints.append(cp.sum(x) == 1)
   prob = cp.Problem(obj, constraints)
   prob.solve()
   return x.value.reshape((-1, 1))
 
 
+def _max_entropy_symmetric_nash_avt(p_mat, num_agents, num_tasks, eps=1e-9):
+  """Solves for the maxent symmetric nash for symmetric 2P zero-sum games,
+    for agent-vs-task cases.
 
+    Using convex programming:
+      min x^Tlog(x) + y^Tlog(y)
+      s.t.
+      x >= 0
+      1^T * x = 1
+      y >= 0
+      1^T * y = 1
+      forall s, such that s has exactly one unit mass on an agent strategy
+      and one unit mass on a task strategy,
+      s^T*p_mat*z <= 0, where z = [x, y], since game-value is 0.
+
+  Args:
+    p_mat: an N*N anti-symmetric payoff matrix for the row player
+    num_agents: number of agents
+    num_tasks: number of tasks
+    eps: minimum probability threshold
+
+  Returns:
+    (x*, y*): a maxent symmetric nash
+  """
+  assert np.array_equal(p_mat, -p_mat.T) and eps >= 0 and eps <= 0.5
+  n = len(p_mat)
+  assert n == num_agents + num_tasks
+  x = cp.Variable(shape=num_agents)
+  y = cp.Variable(shape=num_tasks)
+  z = cp.hstack([x, y])
+  obj = cp.Maximize(cp.sum(cp.entr(z)))
+  constraints = [x >= eps * np.ones(num_agents), cp.sum(x) == 1,
+                 y >= eps * np.ones(num_tasks), cp.sum(y) == 1]
+
+  dev_payoffs = p_mat @ z
+  for a_idx in range(num_agents):
+    for t_idx in range(num_tasks):
+      pure_strategy = np.zeros(n)
+      pure_strategy[a_idx] = 1
+      pure_strategy[num_agents + t_idx] = 1
+      pure_strategy = pure_strategy.reshape((1, -1))
+      constraints.append(pure_strategy @ dev_payoffs <= 0)
+
+  prob = cp.Problem(obj, constraints)
+  prob.solve()
+  return x.value.reshape((-1, 1)), y.value.reshape((-1, 1))
 
 
 def nash_averaging_avt_matrix(s_mat, eps=0.0):
@@ -78,13 +113,13 @@ def nash_averaging_avt_matrix(s_mat, eps=0.0):
     nash_avg_score: the expected payoff under maxent_nash
   """
   m, n = s_mat.shape
-  min_payoffs = np.min(s_mat, axis=1).reshape((m, 1))
-  max_payoffs = np.max(s_mat, axis=1).reshape((m, 1))
+  min_payoffs = np.min(s_mat, axis=0)
+  max_payoffs = np.max(s_mat, axis=0)
   std_p_mat = (s_mat - min_payoffs)/(max_payoffs-min_payoffs)
   a_mat = np.block([[np.zeros(shape=(m, m)), std_p_mat],
                     [-std_p_mat.T, np.zeros(shape=(n, n))]])
-  maxent_nash = np.array(_max_entropy_symmetric_nash(a_mat, num_tasks=n, eps=eps))
-  pa, pe = maxent_nash[:m], maxent_nash[m:]
+  pa, pe = np.array(_max_entropy_symmetric_nash_avt(
+      a_mat, num_agents=m, num_tasks=n, eps=eps))
   return (pa, pe), (std_p_mat.dot(pe), -std_p_mat.T.dot(pa))
 
 

open_spiel/python/algorithms/nash_averaging_test.py
@@ -35,11 +35,31 @@ game_rps = pyspiel.create_matrix_game(
 eq_rps = np.asarray([1 / 3, 1 / 3, 1 / 3])
 value_rps = np.asarray([0., 0., 0.])
 
-# game with one dominated strategy
+# game with one dominated strategy (AvA case)
 p_mat0 = np.asarray([[0.0, 234., 34., -270.], [-234., 0., -38., -464.],
                      [-34., 38., 0., -270.], [270., 464., 270., 0.]])
 game0 = pyspiel.create_matrix_game(p_mat0, -p_mat0)
 dominated_idxs0 = [0, 1, 2]
+
+
+# game with one dominated strategy (AvT case)
+p_mat1 = np.asarray([[0.0, 0.0, 0.0],
+                     [1.0, 10.0, 100.0],
+                     [2.0, 20.0, 200.0],
+                     [3.0, 30.0, 300.0]])
+game1 = pyspiel.create_matrix_game(p_mat1, -p_mat1)
+dominated_idxs1 = [0, 1, 2]
+
+
+
+# game with one multiple dominant strategy (AvT case)
+p_mat2 = np.asarray([[0.0, 0.0, 0.0],
+                     [1.0, 10.0, 100.0],
+                     [2.0, 20.0, 200.0],
+                     [3.0, 30.0, 300.0],
+                     [3.0, 30.0, 300.0]])
+game2 = pyspiel.create_matrix_game(p_mat2, -p_mat2)
+dom_idxs2 = [3, 4]
 
 
 class NashAveragingTest(parameterized.TestCase):
@@ -59,12 +79,27 @@ class NashAveragingTest(parameterized.TestCase):
 
   @parameterized.named_parameters(
       ("game0", game0, dominated_idxs0),)
-  def test_games_with_dominated_strategy(self, game, dom_idxs0):
+  def test_ava_games_with_dominated_strategy(self, game, dominated_idxs):
     maxent_nash, _ = nash_averaging(game)
     with self.subTest("dominated strategies have zero Nash probs"):
-      for idx in dom_idxs0:
+      for idx in dominated_idxs:
         self.assertAlmostEqual(maxent_nash[idx].item(), 0.0)
 
+  @parameterized.named_parameters(
+      ("game1", game1, dominated_idxs1),)
+  def test_avt_games_with_dominated_strategy(self, game, dominated_idxs):
+    (agent_strategy, _), _ = nash_averaging(game, a_v_a=False)
+    with self.subTest("dominated strategies have zero Nash probs"):
+      for idx in dominated_idxs:
+        self.assertAlmostEqual(agent_strategy[idx].item(), 0.0)
+
+  @parameterized.named_parameters(
+      ("game2", game2, dom_idxs2),)
+  def test_avt_games_with_multiple_dominant_strategy(self, game, dom_idxs):
+    (agent_strategy, _), _ = nash_averaging(game, a_v_a=False)
+    with self.subTest("dominated strategies have zero Nash probs"):
+      for idx in dom_idxs:
+        self.assertAlmostEqual(agent_strategy[idx].item(), 1/len(dom_idxs2))
 
 if __name__ == "__main__":
   absltest.main()