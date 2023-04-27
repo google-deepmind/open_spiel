f0d30ac5626aefedd26c259ea3a87b6a66129699
axel <axel.brunnbauer@gmx.at>
2023-04-27 16:20:46 +0200

    - Fix linting issues - Update JAX installation target

--- open_spiel/python/environments/iterated_matrix_game.py
+++ open_spiel/python/environments/iterated_matrix_game.py
@@ -30,7 +30,7 @@ class IteratedMatrixGame(Environment):
     self._t = 0
     self._actions = np.arange(np.prod(
       self.action_spec()['num_actions'])).reshape(
-        *[payoff_matrix.shape[p] for p in range(self._num_players)])
+      *[payoff_matrix.shape[p] for p in range(self._num_players)])
 
   def one_hot(self, x, n):
     return np.eye(n)[x]
@@ -40,23 +40,32 @@ class IteratedMatrixGame(Environment):
     return self._num_players
 
   def observation_spec(self):
-    return dict(info_state=tuple([
-      np.prod(self._payoff_matrix.shape[:-1]) + 1 +
-      (1 if self._include_remaining_iterations else 0)
-    ] for _ in range(self._num_players)),
-                legal_actions=tuple([
-                  self._payoff_matrix.shape[p] for p in range(self._num_players)
-                ]),
-                current_player=())
+    info_state_spec, legal_actions_spec = [], []
+    for i in range(self._num_players):
+      num_actions = np.prod(self._payoff_matrix.shape[:-1]) + 1
+      if self._include_remaining_iterations:
+        num_actions += 1
+      info_state_spec.append([num_actions])
+      legal_actions_spec.append(self._payoff_matrix.shape[i])
+    return {
+      'info_state': tuple(info_state_spec),
+      'legal_actions': tuple(legal_actions_spec),
+      'current_player': ()
+    }
 
   def action_spec(self):
-    return dict(
-      num_actions=tuple(
-        [self._payoff_matrix.shape[p] for p in range(self._num_players)]),
-      min=tuple([0 for p in range(self._num_players)]),
-      max=tuple(
-        [self._payoff_matrix.shape[p] - 1 for p in range(self._num_players)]),
-      dtype=int)
+    num_actions, mins, maxs = [], [], []
+    for i in range(self._num_players):
+      num_actions.append(self._payoff_matrix.shape[i])
+      mins.append(0)
+      maxs.append(self._payoff_matrix.shape[i] - 1)
+
+    return {
+      'num_actions': tuple(num_actions),
+      'min': tuple(mins),
+      'max': tuple(maxs),
+      'dtype': int
+    }
 
   def step(self, actions: np.ndarray):
     if actions.ndim == 1:
@@ -83,18 +92,26 @@ class IteratedMatrixGame(Environment):
       info_state = np.concatenate([
         info_state,
         np.full((self._batch_size, 1), fill_value=remaining_iters)
-      ],
-                                  axis=-1)
-    return TimeStep(observations=dict(
-      info_state=info_state,
-      legal_actions=np.array(
-        [[np.arange(self.action_spec()['num_actions'][p])] * self._batch_size
-         for p in range(self.num_players)]),
-      batch_size=actions.shape[0],
-      current_player=PlayerId.SIMULTANEOUS),
-                    rewards=rewards,
-                    discounts=discounts,
-                    step_type=step_type)
+      ], axis=-1)
+
+    legal_actions = self._get_legal_actions()
+    return TimeStep(
+      observations={
+        'info_state': info_state,
+        'legal_actions': legal_actions,
+        'batch_size': actions.shape[0],
+        'current_player': PlayerId.SIMULTANEOUS},
+      rewards=rewards,
+      discounts=discounts,
+      step_type=step_type
+    )
+
+  def _get_legal_actions(self):
+    legal_actions = []
+    for p in range(self.num_players):
+      actions = np.arange(self.action_spec()['num_actions'][p])
+      legal_actions.append([actions] * self._batch_size)
+    return np.array(legal_actions)
 
   def reset(self):
     self._t = 0
@@ -106,13 +123,12 @@ class IteratedMatrixGame(Environment):
     rewards = np.squeeze(np.zeros((self.num_players, self._batch_size)))
     discounts = np.squeeze(np.ones((self.num_players, self._batch_size)))
     return TimeStep(
-      observations=dict(
-        info_state=[np.squeeze(s).astype(np.float32) for s in info_state],
-        legal_actions=np.array(
-          [[np.arange(self.action_spec()['num_actions'][p])] * self._batch_size
-           for p in range(self.num_players)]),
-        batch_size=self._batch_size,
-        current_player=PlayerId.SIMULTANEOUS),
+      observations={
+        'info_state': [np.squeeze(s).astype(np.float32) for s in info_state],
+        'legal_actions': self._get_legal_actions(),
+        'batch_size': self._batch_size,
+        'current_player': PlayerId.SIMULTANEOUS
+      },
       rewards=[np.squeeze(a).astype(np.float32) for a in rewards],
       discounts=[np.squeeze(a).astype(np.float32) for a in discounts],
       step_type=StepType.FIRST)
@@ -133,10 +149,3 @@ def IteratedMatchingPennies(iterations: int, batch_size=1):
                             batch_size=batch_size,
                             include_remaining_iterations=False)
 
-
-if __name__ == '__main__':
-  env = IteratedPrisonersDilemma(iterations=10, batch_size=4)
-  ts = env.reset()
-  while not ts.last():
-    ts = env.step(np.random.randint(0, 2, size=(4, 2)))
-    print(ts)
