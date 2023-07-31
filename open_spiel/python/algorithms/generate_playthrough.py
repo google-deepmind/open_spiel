d8b0fe1f170c19f485f82c08c7a0147e1bf9db0f
Marc Lanctot <lanctot@google.com>
2023-07-31 13:33:30 +0000

    Update playthroughs to 6 decimal place precision for float values (float vectors and chance outcomes).
    PiperOrigin-RevId: 552462812
    Change-Id: Ibfa4b5f0865884002946b18b6d94ea53f1d27a3e

--- open_spiel/python/algorithms/generate_playthrough.py
+++ open_spiel/python/algorithms/generate_playthrough.py
@@ -38,6 +38,12 @@ _USE_ACTION_IDS = flags.DEFINE_bool(
     "playthough_use_action_ids", default=True,
     help="Whether to use action names or ids when regenerating playthroughs")
 
+# Precision can differ depending on the system and context where the playthrough
+# is generated versus where they are re-generated for testing purposes. To
+# ensure that tests don't fail due to precision, we set the tolarance
+# accordingly.
+_FLOAT_DECIMAL_PLACES = 6
+
 
 def _escape(x):
   """Returns a newline-free backslash-escaped version of the given string."""
@@ -81,11 +87,16 @@ def _format_matrix(mat):
 
 
 def _format_float(x):
-  return "{:.15g}".format(x)
+  return ("{:." + str(_FLOAT_DECIMAL_PLACES) + "g}").format(x)
 
 
 def _format_float_vector(v):
   return "[" + ", ".join([_format_float(x) for x in v]) + "]"
+
+
+def _format_chance_outcomes(chance_outcomes):
+  return "[" + ", ".join(["({},{})".format(outcome, _format_float(prob))
+                          for (outcome, prob) in chance_outcomes]) + "]"
 
 
 def _format_tensor(tensor, tensor_name, max_cols=120):
@@ -384,7 +395,8 @@ def playthrough_lines(game_string, alsologtostdout=False, action_sequence=None,
     if state.is_terminal():
       break
     if state.is_chance_node():
-      add_line("ChanceOutcomes() = {}".format(state.chance_outcomes()))
+      add_line("ChanceOutcomes() = {}".format(
+          _format_chance_outcomes(state.chance_outcomes())))
     if state.is_mean_field_node():
       add_line("DistributionSupport() = {}".format(
           state.distribution_support()))
@@ -517,7 +529,11 @@ def replay(filename):
 
 def update_path(path, shard_index=0, num_shards=1):
   """Regenerates all playthroughs in the path."""
-  for filename in sorted(os.listdir(path))[shard_index::num_shards]:
+  if os.path.isfile(path):
+    file_list = [path]
+  else:
+    file_list = sorted(os.listdir(path))
+  for filename in file_list[shard_index::num_shards]:
     try:
       original, kwargs = _read_playthrough(os.path.join(path, filename))
       try:
