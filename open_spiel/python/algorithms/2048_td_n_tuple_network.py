0275a4b77a78c5270a774e8de910e95e368acbc2
Jazeem Basheer <jazeem23@gmail.com>
2023-08-09 17:28:02 +0530

    Minor changes

--- open_spiel/python/algorithms/2048_td_n_tuple_network.py
+++ open_spiel/python/algorithms/2048_td_n_tuple_network.py
@@ -1,3 +1,19 @@
+# Copyright 2023 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""TD Learning with N-Tuple Networks for 2048"""
+
 from absl import app
 from absl import flags
 from absl import logging
@@ -6,7 +22,7 @@ import numpy as np
 import pyspiel
 
 flags.DEFINE_string("game", "2048", "Name of the game.")
-flags.DEFINE_integer("num_train_episodes", int(1e4),
+flags.DEFINE_integer("num_train_episodes", 15000,
                      "Number of training episodes.")
 flags.DEFINE_integer(
     "eval_every", 1000,
@@ -21,7 +37,7 @@ n_tuple_network_size = len(tuple_paths)
 
 vector_shape = (n_tuple_network_size,) + (max_tuple_index,) * n_tuple_size
 look_up_table = np.zeros(vector_shape)
-alpha = 0.1
+alpha = 0.02
 
 def main(argv):
     game = pyspiel.load_game(FLAGS.game) 
@@ -31,7 +47,6 @@ def main(argv):
     for ep in range(FLAGS.num_train_episodes):
         state = game.new_initial_state()   
         states_in_episode = []            
-
         while not state.is_terminal():            
             if state.is_chance_node():
                 outcomes = state.chance_outcomes()            
@@ -55,7 +70,9 @@ def main(argv):
         sum_rewards += state.returns()[0]
         if (ep + 1) % FLAGS.eval_every == 0:
             logging.info(f"[{ep + 1}] Average Score: {int(sum_rewards / FLAGS.eval_every)}, Max Score: {int(max_score)}, Largest Tile Reached: {int(largest_tile)}")            
-            sum_rewards = 0           
+            sum_rewards = 0
+            largest_tile = 0
+            max_score = 0
 
 def learn(states):
     target = 0
@@ -64,20 +81,17 @@ def learn(states):
         error = target - value(state)
         target = state.rewards()[0] + update(state, alpha * error)
 
-def update(state, u):
-    adjust = u / n_tuple_network_size
+def update(state, adjust):
     value = 0
     for idx, path in enumerate(tuple_paths):
         value += update_tuple(idx, path, state, adjust)
     return value
 
 def update_tuple(idx, path, state, adjust):
-    value = 0
     observation_tensor = state.observation_tensor(0)
     index = (idx,) + tuple([0 if observation_tensor[tile] == 0 else int(np.log2(observation_tensor[tile])) for tile in path])
     look_up_table[index] += adjust
-    value += look_up_table[index]
-    return value
+    return look_up_table[index]
 
 def evaluator(state, action):
     working_state = state.clone()
