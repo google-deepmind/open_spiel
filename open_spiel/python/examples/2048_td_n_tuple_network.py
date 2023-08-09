e8991bf26d44a06f6bbe7541b642f865ac751871
Jazeem Basheer <jazeem23@gmail.com>
2023-08-09 17:56:35 +0530

    Variable renames

--- open_spiel/python/examples/2048_td_n_tuple_network.py
+++ open_spiel/python/examples/2048_td_n_tuple_network.py
@@ -31,9 +31,9 @@ FLAGS = flags.FLAGS
 
 n_tuple_size = 6
 max_tuple_index = 15
-tuple_paths = [[0, 1, 2, 3, 4, 5],[4, 5, 6, 7, 8, 9],
+n_tuples = [[0, 1, 2, 3, 4, 5],[4, 5, 6, 7, 8, 9],
     [0, 1, 2, 4, 5, 6],[4, 5, 6, 8, 9, 10],]
-n_tuple_network_size = len(tuple_paths)
+n_tuple_network_size = len(n_tuples)
 
 look_up_table_shape = (n_tuple_network_size,) + (max_tuple_index,) * n_tuple_size
 look_up_table = np.zeros(look_up_table_shape)
@@ -83,13 +83,13 @@ def learn(states):
 
 def update(state, adjust):
     v = 0
-    for idx, path in enumerate(tuple_paths):
-        v += update_tuple(idx, path, state, adjust)
+    for idx, n_tuple in enumerate(n_tuples):
+        v += update_tuple(idx, n_tuple, state, adjust)
     return v
 
-def update_tuple(idx, path, state, adjust):
+def update_tuple(idx, n_tuple, state, adjust):
     observation_tensor = state.observation_tensor(0)
-    index = (idx,) + tuple([0 if observation_tensor[tile] == 0 else int(np.log2(observation_tensor[tile])) for tile in path])
+    index = (idx,) + tuple([0 if observation_tensor[tile] == 0 else int(np.log2(observation_tensor[tile])) for tile in n_tuple])
     look_up_table[index] += adjust
     return look_up_table[index]
 
@@ -101,8 +101,8 @@ def evaluator(state, action):
 def value(state):
     observation_tensor = state.observation_tensor(0)    
     v = 0
-    for idx, tuple_path in enumerate(tuple_paths):
-        lookup_tuple_index = [0 if observation_tensor[tile] == 0 else int(np.log2(observation_tensor[tile])) for tile in tuple_path]
+    for idx, n_tuple in enumerate(n_tuples):
+        lookup_tuple_index = [0 if observation_tensor[tile] == 0 else int(np.log2(observation_tensor[tile])) for tile in n_tuple]
         lookup_index = (idx,) + tuple(lookup_tuple_index)        
         v += look_up_table[lookup_index]
     return v
