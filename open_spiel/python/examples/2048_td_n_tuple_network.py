ca944729022d628d9a4e41065368850ff743751b
Jazeem Basheer <jazeem23@gmail.com>
2023-08-10 16:01:57 +0530

    Fixed line lengths going above 80 chars

--- open_spiel/python/examples/2048_td_n_tuple_network.py
+++ open_spiel/python/examples/2048_td_n_tuple_network.py
@@ -35,7 +35,8 @@ n_tuples = [[0, 1, 2, 3, 4, 5],[4, 5, 6, 7, 8, 9],
     [0, 1, 2, 4, 5, 6],[4, 5, 6, 8, 9, 10],]
 n_tuple_network_size = len(n_tuples)
 
-look_up_table_shape = (n_tuple_network_size,) + (max_tuple_index,) * n_tuple_size
+look_up_table_shape = (
+    n_tuple_network_size,) + (max_tuple_index,) * n_tuple_size
 look_up_table = np.zeros(look_up_table_shape)
 alpha = 0.02
 
@@ -55,7 +56,8 @@ def main(argv):
                 state.apply_action(action)
             else:
                 legal_actions = state.legal_actions(state.current_player())
-                best_action = max(legal_actions, key=lambda action: evaluator(state, action))
+                best_action = max(legal_actions, 
+                    key=lambda action: evaluator(state, action))
                 state.apply_action(best_action)
                 states_in_episode.append(state.clone())
 
@@ -69,7 +71,12 @@ def main(argv):
         learn(states_in_episode)
         
         if (ep + 1) % FLAGS.eval_every == 0:
-            logging.info(f"[{ep + 1}] Average Score: {int(sum_rewards / FLAGS.eval_every)}, Max Score: {int(max_score)}, Largest Tile Reached: {int(largest_tile)}")            
+            logging.info(
+                f"[{ep + 1}] "
+                f"Average Score: {int(sum_rewards / FLAGS.eval_every)}, "
+                f"Max Score: {int(max_score)}, "
+                f"Largest Tile Reached: {int(largest_tile)}"
+            )
             sum_rewards = 0
             largest_tile = 0
             max_score = 0
@@ -89,7 +96,8 @@ def update(state, adjust):
 
 def update_tuple(idx, n_tuple, state, adjust):
     observation_tensor = state.observation_tensor(0)
-    index = (idx,) + tuple([0 if observation_tensor[tile] == 0 else int(np.log2(observation_tensor[tile])) for tile in n_tuple])
+    index = (idx,) + tuple([0 if observation_tensor[tile] == 0 
+        else int(np.log2(observation_tensor[tile])) for tile in n_tuple])
     look_up_table[index] += adjust
     return look_up_table[index]
 
@@ -102,7 +110,8 @@ def value(state):
     observation_tensor = state.observation_tensor(0)    
     v = 0
     for idx, n_tuple in enumerate(n_tuples):
-        lookup_tuple_index = [0 if observation_tensor[tile] == 0 else int(np.log2(observation_tensor[tile])) for tile in n_tuple]
+        lookup_tuple_index = [0 if observation_tensor[tile] == 0 
+            else int(np.log2(observation_tensor[tile])) for tile in n_tuple]
         lookup_index = (idx,) + tuple(lookup_tuple_index)        
         v += look_up_table[lookup_index]
     return v
