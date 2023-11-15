65d82f27ca9caea414e32dd5b30e7a2395b91654
James Flynn <jameswflynn@outlook.com>
2023-11-15 01:08:16 +0000

    Moved test variables to setUp

--- open_spiel/python/algorithms/efr_test.py
+++ open_spiel/python/algorithms/efr_test.py
@@ -20,49 +20,50 @@ import numpy as np
 
 from open_spiel.python import policy
 from open_spiel.python.algorithms import expected_game_score
-from open_spiel.python.algorithms import cfr
+from open_spiel.python.algorithms import efr
 import pyspiel
-import efr
 
 
-_KUHN_GAME = pyspiel.load_game("kuhn_poker")
-_LEDUC_GAME = pyspiel.load_game("leduc_poker")
 
-_KUHN_UNIFORM_POLICY = policy.TabularPolicy(_KUHN_GAME)
-_LEDUC_UNIFORM_POLICY = policy.TabularPolicy(_LEDUC_GAME)
-_DEVIATIONS_ = ["blind action", "informed action", "blind cf", "informed cf", "bps", "cfps", "csps", "tips", "bhv"]
 
 class EFRTest(parameterized.TestCase, absltest.TestCase):
 
-  @parameterized.parameters(_DEVIATIONS_)
+  def setUp(self):
+    self._KUHN_GAME = pyspiel.load_game("kuhn_poker")
+    self._LEDUC_GAME = pyspiel.load_game("leduc_poker")
+    self._KUHN_3P_GAME = pyspiel.load_game("kuhn_poker(players=3)")
+    self._SHERIFF_GAME = pyspiel.load_game("sheriff")
+
+    self._KUHN_UNIFORM_POLICY = policy.TabularPolicy(self._KUHN_GAME)
+    self._LEDUC_UNIFORM_POLICY = policy.TabularPolicy(self._LEDUC_GAME)
+
+  @parameterized.parameters(["blind action", "informed action", "blind cf", "informed cf", "bps", "cfps", "csps", "tips", "bhv"])
   def test_policy_zero_is_uniform(self, deviations_name):
     # We use Leduc and not Kuhn, because Leduc has illegal actions and Kuhn does
     # not.
-    game = pyspiel.load_game("leduc_poker")
     cfr_solver = efr.EFRSolver(
-        game=game,
+        game=self._LEDUC_GAME,
         deviations_name=deviations_name
         )
     np.testing.assert_array_equal(
-        _LEDUC_UNIFORM_POLICY.action_probability_array,
+        self._LEDUC_UNIFORM_POLICY.action_probability_array,
         cfr_solver.current_policy().action_probability_array)
     np.testing.assert_array_equal(
-        _LEDUC_UNIFORM_POLICY.action_probability_array,
+        self._LEDUC_UNIFORM_POLICY.action_probability_array,
         cfr_solver.average_policy().action_probability_array)
 
   @parameterized.parameters(
       ["blind cf", "informed cf", "bps", "cfps", "csps", "tips", "bhv"])
   def test_efr_kuhn_poker(self, deviations_name):
-    game = pyspiel.load_game("kuhn_poker")
     efr_solver = efr.EFRSolver(
-        game=game,
+        game=self._KUHN_GAME,
         deviations_name=deviations_name
         )
     for _ in range(300):
       efr_solver.evaluate_and_update_policy()
     average_policy = efr_solver.average_policy()
     average_policy_values = expected_game_score.policy_value(
-        game.new_initial_state(), [average_policy] * 2)
+        self._KUHN_GAME.new_initial_state(), [average_policy] * 2)
     # 1/18 is the Nash value. See https://en.wikipedia.org/wiki/Kuhn_poker
     np.testing.assert_allclose(
         average_policy_values, [-1 / 18, 1 / 18], atol=1e-3)
@@ -70,9 +71,8 @@ class EFRTest(parameterized.TestCase, absltest.TestCase):
   @parameterized.parameters(
       ["blind cf", "informed cf", "bps", "cfps", "csps", "tips", "bhv"])
   def test_efr_kuhn_poker_3p(self, deviations_name):
-    game = pyspiel.load_game("kuhn_poker(players=3)")
     efr_solver = efr.EFRSolver(
-        game=game,
+        game=self._KUHN_3P_GAME,
         deviations_name=deviations_name
     )
     strategies = []
@@ -84,7 +84,7 @@ class EFRTest(parameterized.TestCase, absltest.TestCase):
       strategies.append(policy.python_policy_to_pyspiel_policy(
           efr_solver.current_policy()))
       corr_dev = pyspiel.uniform_correlation_device(strategies)
-      cce_dist_info = pyspiel.cce_dist(game, corr_dev)
+      cce_dist_info = pyspiel.cce_dist(self._KUHN_3P_GAME, corr_dev)
       corr_dist_values.append(cce_dist_info.dist_value)
     self.assertLess(corr_dist_values[-1], corr_dist_values[0])
   
@@ -92,13 +92,10 @@ class EFRTest(parameterized.TestCase, absltest.TestCase):
   @parameterized.parameters(
       ["blind cf", "bps", "tips"])
   def test_efr_cce_dist_sheriff(self, deviations_name):
-    game = pyspiel.load_game("sheriff")
     efr_solver = efr.EFRSolver(
-        game=game,
+        game=self._SHERIFF_GAME,
         deviations_name=deviations_name
     )   
-    #efr_solver = cfr.CFRSolver(game)
-
     strategies = []
     corr_dist_values = []
     for _ in range(5):
@@ -106,7 +103,7 @@ class EFRTest(parameterized.TestCase, absltest.TestCase):
       strategies.append(policy.python_policy_to_pyspiel_policy(
           efr_solver.current_policy()))      
       corr_dev = pyspiel.uniform_correlation_device(strategies)
-      cce_dist_info = pyspiel.cce_dist(game, corr_dev)
+      cce_dist_info = pyspiel.cce_dist(self._SHERIFF_GAME, corr_dev)
       corr_dist_values.append(cce_dist_info.dist_value)
     self.assertLess(corr_dist_values[-1], corr_dist_values[0])
 if __name__ == "__main__":
