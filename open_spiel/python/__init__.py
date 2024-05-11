d52168c3d628d630a28cc88c5769d87d7568b94b
lanctot <lanctot@google.com>
2024-05-11 22:21:46 +0000

    Add GameParameter type to open_spiel/python/__init__.py

--- open_spiel/python/__init__.py
+++ open_spiel/python/__init__.py
@@ -12,3 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Dict, Union
+GameParameter = Union[int, float, str, bool, Dict[str, 'GameParameter']]
