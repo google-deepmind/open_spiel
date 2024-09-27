d5a5bdc3b1d9888bad8d7afa57b46051e7199cf7
Marc Lanctot <lanctot@google.com>
2024-09-27 19:21:38 +0000

    Improve count_all_states example by adding command-line argument rather than a loop over a fixed set of games.
    PiperOrigin-RevId: 679685589
    Change-Id: I499054cc8d23f63f9957cb6f6829e5a8c040eeae

--- open_spiel/examples/count_all_states.cc
+++ open_spiel/examples/count_all_states.cc
@@ -12,18 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <iostream>
+#include <memory>
 #include <string>
+#include <vector>
 
+#include "open_spiel/abseil-cpp/absl/flags/flag.h"
+#include "open_spiel/abseil-cpp/absl/flags/parse.h"
 #include "open_spiel/abseil-cpp/absl/container/flat_hash_set.h"
-#include "open_spiel/abseil-cpp/absl/strings/str_format.h"
 #include "open_spiel/algorithms/get_all_histories.h"
-#include "open_spiel/canonical_game_strings.h"
 #include "open_spiel/spiel.h"
+#include "open_spiel/spiel_globals.h"
 #include "open_spiel/spiel_utils.h"
 
+ABSL_FLAG(std::string, game_string, "kuhn_poker", "Game to count states for.");
 
-using open_spiel::TurnBasedGoofspielGameString;
-using open_spiel::LoadGame;
+
 using open_spiel::GameType;
 using open_spiel::StateType;
 using open_spiel::algorithms::GetAllHistories;
@@ -38,50 +42,46 @@ using open_spiel::algorithms::GetAllHistories;
 //     applied), e.g. in tic-tac-toe the current state of the board, regardless
 //     of the order in which the moves were played.
 int main(int argc, char** argv) {
-  for (const std::string& game_name :
-       {std::string("tic_tac_toe"), std::string("kuhn_poker"),
-        std::string("leduc_poker"), std::string("liars_dice"),
-        TurnBasedGoofspielGameString(4), TurnBasedGoofspielGameString(5),
-        TurnBasedGoofspielGameString(6)}) {
-    std::shared_ptr<const open_spiel::Game> game =
-        LoadGame(std::string(game_name));
-    std::vector<std::unique_ptr<open_spiel::State>> all_histories =
-        GetAllHistories(*game, /*depth_limit=*/-1, /*include_terminals=*/true,
-                        /*include_chance_states=*/true);
-    absl::flat_hash_set<std::string> nonterminal_states;
-    absl::flat_hash_set<std::string> terminal_states;
-    const int num_histories = all_histories.size();
-    int num_terminal_histories = 0;
-    int num_chance_nodes = 0;
-    for (const auto& state : all_histories) {
-      switch (state->GetType()) {
-        case StateType::kDecision:
-          if (game->GetType().information ==
-              GameType::Information::kPerfectInformation) {
-            nonterminal_states.insert(state->ToString());
-          } else {
-            nonterminal_states.insert(state->InformationStateString());
-          }
-          break;
-        case StateType::kTerminal:
-          ++num_terminal_histories;
-          terminal_states.insert(state->ToString());
-          break;
-        case StateType::kChance:
-          ++num_chance_nodes;
-          break;
-        case StateType::kMeanField:
-          open_spiel::SpielFatalError("kMeanField not handeled.");
-      }
+  absl::ParseCommandLine(argc, argv);
+  std::string game_name = absl::GetFlag(FLAGS_game_string);
+  std::shared_ptr<const open_spiel::Game> game =
+      open_spiel::LoadGame(absl::GetFlag(game_name));
+  std::vector<std::unique_ptr<open_spiel::State>> all_histories =
+      GetAllHistories(*game, /*depth_limit=*/-1, /*include_terminals=*/true,
+                      /*include_chance_states=*/true);
+  absl::flat_hash_set<std::string> nonterminal_states;
+  absl::flat_hash_set<std::string> terminal_states;
+  const int num_histories = all_histories.size();
+  int num_terminal_histories = 0;
+  int num_chance_nodes = 0;
+  for (const auto& state : all_histories) {
+    switch (state->GetType()) {
+      case StateType::kDecision:
+        if (game->GetType().information ==
+            GameType::Information::kPerfectInformation) {
+          nonterminal_states.insert(state->ToString());
+        } else {
+          nonterminal_states.insert(state->InformationStateString());
+        }
+        break;
+      case StateType::kTerminal:
+        ++num_terminal_histories;
+        terminal_states.insert(state->ToString());
+        break;
+      case StateType::kChance:
+        ++num_chance_nodes;
+        break;
+      case StateType::kMeanField:
+        open_spiel::SpielFatalError("kMeanField not handeled.");
     }
-    const int num_nonterminal_states = nonterminal_states.size();
-    const int num_terminal_states = terminal_states.size();
-    std::cout << "Game: " << game_name
-              << ", num_histories: " << num_histories
-              << ", num_terminal_histories: " << num_terminal_histories
-              << ", num_chance_nodes: " << num_chance_nodes
-              << ", num_nonterminal_states: " << num_nonterminal_states
-              << ", num_terminal_states: " << num_terminal_states
-              << std::endl;
   }
+  const int num_nonterminal_states = nonterminal_states.size();
+  const int num_terminal_states = terminal_states.size();
+  std::cout << "Game: " << game_name
+            << ", num_histories: " << num_histories
+            << ", num_terminal_histories: " << num_terminal_histories
+            << ", num_chance_nodes: " << num_chance_nodes
+            << ", num_nonterminal_states: " << num_nonterminal_states
+            << ", num_terminal_states: " << num_terminal_states
+            << std::endl;
 }
