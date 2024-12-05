52ad2d8fa902ad49cb76172f4abcd6641a38e12f
lanctot <lanctot@google.com>
2024-12-05 14:42:46 +0000

    Add comment

--- open_spiel/scripts/ci_python_prechecks.sh
+++ open_spiel/scripts/ci_python_prechecks.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Only use for Github Actions CI!
 OS=`uname -a | awk '{print $1}'`
 if [[ "$OS" = "Darwin" ]]; then
   # This seems to be necessary to install python via brew in Github Actions
