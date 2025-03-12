077cda0a22d1606d16305f1b41b6ac9d3dccb7f0
Ian Gemp <imgemp@google.com>
2025-03-12 10:20:16 +0000

    Generalize sequence form utils to n-player, general-sum. Include method to construct per-player sequence-form constraint arrays (Ax = b).
    PiperOrigin-RevId: 736067839
    Change-Id: Idc866ce35fc4dd192d7bf6d58029f8200b8410a4

--- open_spiel/python/algorithms/sequence_form_utils_test.py
+++ open_spiel/python/algorithms/sequence_form_utils_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for open_spiel.python.sequence_form_utils.py."""
+"""Tests several sequence form utilities."""
 
 from absl.testing import absltest
 from absl.testing import parameterized
@@ -21,8 +21,8 @@ import numpy as np
 
 from open_spiel.python import policy
 from open_spiel.python.algorithms import cfr
+from open_spiel.python.algorithms import expected_game_score as egs
 from open_spiel.python.algorithms import sequence_form_utils
-from open_spiel.python.algorithms.expected_game_score import policy_value
 import pyspiel
 
 _KUHN_GAME = pyspiel.load_game('kuhn_poker')
@@ -76,13 +76,15 @@ class SequenceFormTest(parameterized.TestCase):
      _) = sequence_form_utils.construct_vars(game)
 
     uniform_policies = policy.TabularPolicy(game)
-    uniform_value = policy_value(game.new_initial_state(),
-                                 [uniform_policies, uniform_policies])
+    uniform_value = egs.policy_value(
+        game.new_initial_state(), [uniform_policies, uniform_policies]
+    )
     sequences = sequence_form_utils.policy_to_sequence(game, uniform_policies,
                                                        infoset_actions_to_seq)
+    min_mat = -payoff_mat[0]
     np.testing.assert_allclose(
         uniform_value[0],
-        -sequences[0].T @ payoff_mat @ sequences[1],
+        -sequences[0].T @ min_mat @ sequences[1],
         rtol=1e-10)
 
     # use cfr iterations to construct new policy
@@ -91,11 +93,56 @@ class SequenceFormTest(parameterized.TestCase):
       cfr_solver.evaluate_and_update_policy()
 
     policies = cfr_solver.average_policy()
-    cfr_value = policy_value(game.new_initial_state(), [policies, policies])
+    cfr_value = egs.policy_value(game.new_initial_state(), [policies, policies])
     sequences = sequence_form_utils.policy_to_sequence(game, policies,
                                                        infoset_actions_to_seq)
     np.testing.assert_allclose(
-        cfr_value[0], -sequences[0].T @ payoff_mat @ sequences[1], rtol=1e-10)
+        cfr_value[0], -sequences[0].T @ min_mat @ sequences[1], rtol=1e-10)
+
+  @parameterized.parameters(
+      {
+          'game': _KUHN_GAME,
+          'seed': 12345
+      },
+      {
+          'game': _LEDUC_GAME,
+          'seed': 12345
+      },
+  )
+  def test_sequence_tangent_projection(self, game, seed, step_size=1e-2):
+    (_, infoset_actions_to_seq, infoset_action_maps, infoset_parent_map, _,
+     _) = sequence_form_utils.construct_vars(game)
+
+    uniform_policies = policy.TabularPolicy(game)
+    sequences = sequence_form_utils.policy_to_sequence(game, uniform_policies,
+                                                       infoset_actions_to_seq)
+
+    constraints = sequence_form_utils.construct_constraint_vars(
+        infoset_parent_map, infoset_actions_to_seq, infoset_action_maps)
+
+    rnd = np.random.RandomState(seed)
+
+    con_errs = []
+    for p, (con_mat, b) in constraints.items():
+      seq_p = sequences[p]
+      # generate random sequence-form "gradient" direction
+      grad = rnd.randn(len(seq_p))
+      grad = grad / np.linalg.norm(grad)
+      # construct tangent projection from sequence-form constraints
+      pinv_con_mat = np.linalg.pinv(con_mat)
+      proj = np.eye(con_mat.shape[1]) - pinv_con_mat.dot(con_mat)
+      # project gradient onto tangent space
+      grad_proj = proj.dot(grad)
+      # take gradient step (should remain on treeplex w/ small enough step_size)
+      seq_p_new = seq_p + step_size * grad_proj
+      # measure constraint violation error after gradient step (should be ~0)
+      con_err = np.linalg.norm(con_mat.dot(seq_p_new) - b)
+      con_errs.append(con_err)
+
+    np.testing.assert_allclose(
+        con_errs,
+        np.zeros(len(con_errs)),
+        atol=1e-10)
 
 
 if __name__ == '__main__':
