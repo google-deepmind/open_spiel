5759bfba72bda633ade94169f980479e0cecc5bb
Piotr Zielinski <zielinski@google.com>
2025-03-19 11:27:25 +0000

    Add InfoStateString + state resampling to Blackjack
    PiperOrigin-RevId: 738341460
    Change-Id: I1660ee203010ba1344b82f6176ad25e7ec8b2296

--- open_spiel/games/blackjack/blackjack_test.cc
+++ open_spiel/games/blackjack/blackjack_test.cc
@@ -14,6 +14,10 @@
 
 #include "open_spiel/games/blackjack/blackjack.h"
 
+#include <set>
+#include <string>
+#include <vector>
+
 #include "open_spiel/spiel_utils.h"
 #include "open_spiel/tests/basic_tests.h"
 
@@ -103,12 +107,105 @@ void PlayerBustTest() {
   SPIEL_CHECK_EQ(state->PlayerReturn(0), -1);
 }
 
+void DealersFirstCardHiddenTest() {
+  std::shared_ptr<const Game> game = LoadGame("blackjack");
+  std::unique_ptr<State> state = game->NewInitialState();
+
+  // Deal cards to player.
+  SPIEL_CHECK_TRUE(state->IsChanceNode());
+  state->ApplyAction(9);
+  SPIEL_CHECK_TRUE(state->IsChanceNode());
+  state->ApplyAction(22);
+
+  // Deal cards to dealer.
+  SPIEL_CHECK_TRUE(state->IsChanceNode());
+  state->ApplyAction(8);
+  SPIEL_CHECK_TRUE(state->IsChanceNode());
+  state->ApplyAction(4);
+
+  // Cast state to blackjack state.
+  auto blackjack_state = dynamic_cast<BlackjackState *>(state.get());
+  SPIEL_CHECK_TRUE(blackjack_state != nullptr);
+  std::set<int> visible_cards = blackjack_state->VisibleCards();
+
+  // Dealer's first card.
+  SPIEL_CHECK_TRUE(visible_cards.find(8) == visible_cards.end());
+
+  // Remaining cards.
+  SPIEL_CHECK_TRUE(visible_cards.find(9) != visible_cards.end());
+  SPIEL_CHECK_TRUE(visible_cards.find(22) != visible_cards.end());
+  SPIEL_CHECK_TRUE(visible_cards.find(4) != visible_cards.end());
+}
+
+void InfoStateStringDoesNotContainDealerFirstCardTest() {
+  std::shared_ptr<const Game> game = LoadGame("blackjack");
+  std::unique_ptr<State> state = game->NewInitialState();
+
+  // Deal cards to player.
+  SPIEL_CHECK_TRUE(state->IsChanceNode());
+  state->ApplyAction(9);
+  SPIEL_CHECK_TRUE(state->IsChanceNode());
+  state->ApplyAction(22);
+
+  // Deal cards to dealer.
+  SPIEL_CHECK_TRUE(state->IsChanceNode());
+  state->ApplyAction(8);
+  SPIEL_CHECK_TRUE(state->IsChanceNode());
+  state->ApplyAction(4);
+
+  std::string info_state_string = state->InformationStateString(0);
+  SPIEL_CHECK_EQ(info_state_string, "9 22 4");
+}
+
+void ResamplingHistoryTest() {
+  std::shared_ptr<const Game> game = LoadGame("blackjack");
+  std::unique_ptr<State> state = game->NewInitialState();
+
+  // Deal cards to player.
+  SPIEL_CHECK_TRUE(state->IsChanceNode());
+  state->ApplyAction(9);
+  SPIEL_CHECK_TRUE(state->IsChanceNode());
+  state->ApplyAction(22);
+
+  // Deal cards to dealer.
+  SPIEL_CHECK_TRUE(state->IsChanceNode());
+  state->ApplyAction(8);
+  SPIEL_CHECK_TRUE(state->IsChanceNode());
+  state->ApplyAction(4);
+
+  auto b_original = dynamic_cast<BlackjackState *>(state.get());
+
+  std::vector<float> random_seeds = {0.12345, 0.6123, 0.0101};
+  // Resample from infostate.
+  for (float seed : random_seeds) {
+    std::unique_ptr<State> resampled_state =
+        state->ResampleFromInfostate(0, [seed]() { return seed; });
+
+    auto b_resampled = dynamic_cast<BlackjackState *>(resampled_state.get());
+
+    // All cards should be the same except for the dealer's first card.
+    SPIEL_CHECK_EQ(b_original->cards(0)[0], b_resampled->cards(0)[0]);
+    SPIEL_CHECK_EQ(b_original->cards(0)[0], b_resampled->cards(0)[0]);
+    SPIEL_CHECK_EQ(b_original->cards(1)[1], b_resampled->cards(1)[1]);
+
+    SPIEL_CHECK_NE(b_original->cards(1)[0], b_resampled->cards(1)[0]);
+
+    // Check that dealer's first card is not visible.
+    std::set<int> visible_cards = b_resampled->VisibleCards();
+    SPIEL_CHECK_TRUE(visible_cards.find(b_resampled->cards(1)[0]) ==
+                     visible_cards.end());
+  }
+}
+
 void BasicBlackjackTests() {
   testing::LoadGameTest("blackjack");
   testing::RandomSimTest(*LoadGame("blackjack"), 100);
   NoBustPlayerWinTest();
   PlayerBustTest();
   DealerBustTest();
+  DealersFirstCardHiddenTest();
+  InfoStateStringDoesNotContainDealerFirstCardTest();
+  ResamplingHistoryTest();
 }
 
 }  // namespace
