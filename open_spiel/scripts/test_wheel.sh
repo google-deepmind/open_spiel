519e44e06b33f6d59d9dd76062fbfe37e55b577b
Marc Lanctot <lanctot@google.com>
2025-06-09 20:38:47 -0230

    Update test_wheel.sh

--- open_spiel/scripts/test_wheel.sh
+++ open_spiel/scripts/test_wheel.sh
@@ -65,8 +65,14 @@ if [[ "$MODE" = "full" ]]; then
     ${PYBIN} -m pip install wheelhouse/open_spiel-*-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
   elif [[ "$OS" = "Darwin" && "$OS_PYTHON_VERSION" = "3.9" ]]; then
     ${PYBIN} -m pip install wheelhouse/open_spiel-*-cp39-cp39-macosx_10_9_x86_64.whl
-  else
+  elif [[ "$OS" = "Darwin" && "$OS_PYTHON_VERSION" = "3.11" ]]; then
+    # Run on the xlarge macs for arm64
     ${PYBIN} -m pip install wheelhouse/open_spiel-*-cp311-cp311-macosx_11_0_arm64.whl
+  elif [[ "$OS" = "Darwin" && "$OS_PYTHON_VERSION" = "3.12" ]]; then
+    ${PYBIN} -m pip install wheelhouse/open_spiel-*-cp313-cp313-macosx_10_13_x86_64.whl
+  else
+    echo "Config not found for full tests"
+    exit -1
   fi
 fi
 
