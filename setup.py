d10f0b5924c2cdd3bec8d991a6a1ed41f8299c5a
Marc Lanctot <marc.lanctot@gmail.com>
2025-10-10 14:03:20 -0230

    Release: version 1.6.4

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ else:
 
 setuptools.setup(
     name="open_spiel",
-    version="1.6.3",
+    version="1.6.4",
     license="Apache 2.0",
     author="The OpenSpiel authors",
     author_email="open_spiel@google.com",
