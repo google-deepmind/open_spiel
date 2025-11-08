5af7c5461ce21e865bacf99a53606fb48451a970
Adam Czachorowski <gislan@utumno.pl>
2025-11-08 14:08:05 +0100

    Feed the correct return (for player making the move, not always player 0) when adding states to replay buffer.
    Before this fix all states in the game would be fed to the learning
    process with the outcome for player 0. This led to some funny results
    including model randomly changing sign on it's value estimation for the
    same situation every few training steps.

--- open_spiel/algorithms/alpha_zero_torch/alpha_zero.cc
+++ open_spiel/algorithms/alpha_zero_torch/alpha_zero.cc
@@ -364,9 +364,9 @@ void learner(const open_spiel::Game& game, const AlphaZeroConfig& config,
         outcomes.Add(p1_outcome > 0 ? 0 : (p1_outcome < 0 ? 1 : 2));
 
         for (const Trajectory::State& state : trajectory->states) {
-          replay_buffer.Add(VPNetModel::TrainInputs{state.legal_actions,
-                                                    state.observation,
-                                                    state.policy, p1_outcome});
+          replay_buffer.Add(VPNetModel::TrainInputs{
+              state.legal_actions, state.observation, state.policy,
+              trajectory->returns[state.current_player]});
           num_states += 1;
         }
 
