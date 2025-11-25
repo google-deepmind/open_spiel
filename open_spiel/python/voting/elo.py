14652ba04fab7ddc9d51ef4d56bc310a8176bccb
Marc Lanctot <lanctot@google.com>
2025-11-25 17:55:08 +0000

    Improve Elo computation from PreferenceProfile (specifically, remove hack that requires converting to strings just to use MatchRecord; compute Elo from numpy matrices instead)
    PiperOrigin-RevId: 836711271
    Change-Id: If2406aa6960b609a5fc8b1ab37fb8a53dd09c820

--- open_spiel/python/voting/elo.py
+++ open_spiel/python/voting/elo.py
@@ -14,6 +14,8 @@
 
 """A helpful wrapper for the Elo rating system."""
 
+import numpy as np
+from open_spiel.python.algorithms import elo
 import pyspiel
 from open_spiel.python.voting import base
 
@@ -23,29 +25,36 @@ def compute_ratings_from_preference_profile(
     smoothing_factor: float = pyspiel.elo.DEFAULT_SMOOTHING_FACTOR,
     max_iterations: int = pyspiel.elo.DEFAULT_MAX_ITERATIONS,
     convergence_delta: float = pyspiel.elo.DEFAULT_CONVERGENCE_DELTA,
-) -> dict[str, float]:
+) -> dict[base.AlternativeId, float]:
   """Compute Elo ratings from a win matrix and a draw matrix."""
   options = pyspiel.elo.default_elo_options()
   options.smoothing_factor = smoothing_factor
   options.max_iterations = max_iterations
   options.convergence_delta = convergence_delta
-  match_records = []
+  num_agents = profile.num_alternatives()
+  alt_idx = profile.alternatives_dict
+  win_matrix = np.zeros((num_agents, num_agents), dtype=int)
   for vote in profile.votes:
     for i in range(len(vote.vote)):
       for j in range(i + 1, len(vote.vote)):
         for _ in range(vote.weight):
-          match_records.append(
-              pyspiel.elo.MatchRecord(
-                  str(vote.vote[i]),
-                  str(vote.vote[j]),
-                  pyspiel.elo.MatchOutcome.FIRST_PLAYER_WIN,
-              )
-          )
-  return pyspiel.elo.compute_ratings_from_match_records(match_records, options)
+          agent_i_idx = alt_idx[vote.vote[i]]
+          agent_j_idx = alt_idx[vote.vote[j]]
+          win_matrix[agent_i_idx, agent_j_idx] += 1
+  ratings_array = elo.compute_ratings_from_matrices(
+      win_matrix, smoothing_factor=smoothing_factor,
+      max_iterations=max_iterations,
+      convergence_delta=convergence_delta,
+  )
+  alternatives = profile.alternatives
+  return {alternatives[i]: ratings_array[i] for i in range(num_agents)}
 
 
 class EloVoting(base.AbstractVotingMethod):
-  """Implements Elo as a voting method."""
+  """Implements Elo as a voting method.
+
+  Note: if there is no data on an alternatives, it is assigned a rating of 0.
+  """
 
   def __init__(
       self,
@@ -67,7 +76,12 @@ class EloVoting(base.AbstractVotingMethod):
     )
     sorted_ratings = sorted(ratings_dict.items(), key=lambda item: item[1],
                             reverse=True)
+    if len(sorted_ratings) < profile.num_alternatives():
+      # If missing alternatives, fill them in with zero scores.
+      for alt in profile.alternatives:
+        if alt not in ratings_dict:
+          sorted_ratings.append((alt, 0.0))
+    assert len(sorted_ratings) == profile.num_alternatives()
     outcome = base.RankOutcome()
     outcome.unpack_from(sorted_ratings)
     return outcome
-
