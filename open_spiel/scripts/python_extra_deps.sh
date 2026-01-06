3e8b65815051d71db220cb1aa43165c42e0a9f3c
Sacha Chernyavskiy <work.alexch2@gmail.com>
2026-01-06 16:26:47 +0300

    Wrong merge

--- open_spiel/scripts/python_extra_deps.sh
+++ open_spiel/scripts/python_extra_deps.sh
@@ -73,6 +73,12 @@ elif verlt $PY_VER 3.14; then
   export OPEN_SPIEL_PYTHON_PYTORCH_DEPS="torch==2.9.1"
   export OPEN_SPIEL_PYTHON_JAX_DEPS="jax==0.8.1 jaxlib==0.8.1 flax==0.12.1 dm-haiku==0.0.16 optax==0.2.6 chex==0.1.91 rlax==0.1.8 distrax==0.1.7"
   export OPEN_SPIEL_PYTHON_MISC_DEPS="IPython==8.23.0 networkx==3.3 matplotlib==3.10.7 mock==5.1.0 nashpy==0.0.41 scipy==1.15.2 testresources==2.0.1 cvxopt==1.3.2 cvxpy==1.6.1 ecos==2.0.14 osqp==1.0.4 tensorflow_datasets==4.9.3 pokerkit==0.6.3"
+else
+  # TODO(author5). Added in #1381, but not working yet. Come back to this.
+  echo "Detected Python version 3.14"
+  export OPEN_SPIEL_PYTHON_PYTORCH_DEPS="torch==2.8.0"
+  export OPEN_SPIEL_PYTHON_JAX_DEPS="jax==0.7.2 jaxlib==0.7.2 dm-haiku==0.0.15 optax==0.2.6 chex==0.1.91 rlax==0.1.8 distrax==0.1.7 flax==0.12.0"
+  export OPEN_SPIEL_PYTHON_MISC_DEPS="IPython==8.23.0 networkx==3.3 matplotlib==3.10.7 mock==5.1.0 nashpy==0.0.41 scipy==1.15.2 testresources==2.0.1 cvxopt==1.3.2 cvxpy==1.6.1 ecos==2.0.14 osqp==1.0.4 tensorflow_datasets==4.9.3 pokerkit==0.6.3"
 fi
 
 
