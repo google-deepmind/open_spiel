349a5f5cbff00df3648ae5868c341428cc289d8d
lanctot <lanctot@google.com>
2026-07-16 19:51:33 +0000

    Release 2.0.1

--- setup.py
+++ setup.py
@@ -152,7 +152,7 @@ else:
 
 setuptools.setup(
     name="open_spiel",
-    version="2.0",
+    version="2.0.1",
     license="Apache 2.0",
     author="The OpenSpiel authors",
     author_email="open_spiel@google.com",
